cd183c30e1bcf1b31d47fb89f9b4fe1073c5ddf1
cansmile <cansmile@gmail.com>
2019-02-21 01:41:18 +0900

    Nagehts - 190221
    level A1 Deutsch Lern Site

--- js/taptogroup.js
+++ js/taptogroup.js
@@ -8,19 +8,30 @@ div#lsts>div.itm-lst>.ttl
 
 // 항목 선택하면 토글
 $("button.itm").bind("click", function() {
-	$(this).removeClass("btn-light");
-	$(this).toggleClass("btn-secondary");
+	if($(this).parent().hasClass("itm-lst")) {
+		$(this).blur();
+	} else {
+		$(this).removeClass("btn-light");
+		$(this).toggleClass("btn-secondary");
+	}
+});
+
+$("button").on("click", function () {
+	if($(this).parent().hasClass("1itm")) {
+		$(this).blur();
+		$(this).removeClass("btn-secondary");
+	}
 });
 
 // 제목 클릭하면 선택한 항목 넣기
 $(".ttl").on("click", function() {
 var t = $(this);
-var tn = $(this).parent().attr("id").charAt(4); // 클릭한 주제 번호 얻기
-var wa = 0;
+var tn = $(this).parent().attr("id").substr(4); // 클릭한 주제 번호 얻기
 var tm = 0;
 	
 	// 단어가 주제에 맞는지 대조
 	$(".btn-secondary").each( function () {
+		var tt = $(this);
 		// 단어의 답안 번호 얻어내기
 		for(var i = 1; i <= $(".itm-lst").length; i++) {
 			var c = eval('"ans'+i+'"');
@@ -28,25 +39,35 @@ var tm = 0;
 				var a=i;
 			};
 		};
-		
+
+		// 답안 번호랑 맞는 것만 넣기
 		if(a != tn) {
+				// 틀리면 띠용
 				ion.sound.play("Cartoon_Boing");
+				if($(this).parent().hasClass("itm-lst")) {
+					$(this).blur();
+					$(this).addClass("btn-light");
+				}
 
-				if(wa == 0) {
-					ion.sound.play($(this).attr("id"));
-				} else {
-					tm += 5000;
-					ion.sound.delay(tm).play($(this).attr("id"));
-				};
-				
 		} else {
-			$(this).addClass("btn-block btn-light");
-			$(this).insertAfter(t);
-		}
-	});
+				// 맞으면 넣기
+				$(this).addClass("btn-block btn-light");
+				$(this).insertAfter(t);
 
+				// 한 아이템만 넣는 리스트 상자 클래스 있어?
+				if(t.parent().hasClass("1itm")) {
+					t.remove();
+					$(this).removeClass("itm");
+					$(this).removeClass('btn-secondary');
+				}
+
+		} // 답안 번호랑 맞는 것만 넣기 마감
+
+
+	}); // btn-secondary.each() 마감
+
+	// 선택 모두 풀기
 	$(".itm").removeClass("btn-secondary");
-	
 });
 
 // 되돌리기 누르면 전부 목록으로 넣기

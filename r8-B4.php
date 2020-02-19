24a99ce4bd92996d5428cc154f41128693735cca
cansmile <cansmile@gmail.com>
2020-02-19 21:21:39 +0900

    Nagehts-2020021900001

--- r8-B4.php
+++ r8-B4.php
@@ -331,7 +331,7 @@
 
 					 // 미리 답 넣어놓기
 					var pan = new Array(), pann;
-					pan = [];
+					pan = [15,21];
 					for(var p = 0; p < pan.length; p++) {
 						pann = "#"+pan[p];
 						for(var i = 0; i < $(".itm-lst").length; i++) {

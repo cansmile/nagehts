79492bafafef51aabdbf1861a1bccbd6feba7ed1
cansmile <cansmile@gmail.com>
2020-02-21 19:08:35 +0900

    R8 E2 수정 반영
    답안 배열에서 구두점 제거

--- r8-E2.php
+++ r8-E2.php
@@ -193,7 +193,7 @@
 		$(".tran").hide();
 		$(".ant").hide();
 		var an=new Array();
-		var an=["Fieber", "Grippe", "Kopfschmerzen", "Kopf", "Halsschmerzen", "Mein Hals", "Zahnschmerzen.", "Zahn", "Bauch", "Bauchschmerzen"];
+		var an=["Fieber", "Grippe", "Kopfschmerzen", "Kopf", "Halsschmerzen", "Mein Hals", "Zahnschmerzen", "Zahn", "Bauch", "Bauchschmerzen"];
 		$(document).ready(function() {
 			/* 입력하는 문자 확인(정답 표시 없음) 여기부터 */
 			// 값 확인해보자, io값이 참이면 전체 검사

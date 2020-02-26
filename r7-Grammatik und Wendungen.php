2a21c7d6a44296ba9105e712b841d4442f409eef
cansmile <cansmile@gmail.com>
2020-02-26 18:45:52 +0900

    R7 E4 Grammatik und Wendungen
    음원 추가하고 시간 입력

--- r7-Grammatik und Wendungen.php
+++ r7-Grammatik und Wendungen.php
@@ -11,7 +11,7 @@
 						<div class="card-header" id="heading1">
 							<h5 class="mb-0">
 								<button class="btn btn-outline-danger mt-1 mx-1 btn-block" type="button" data-toggle="collapse" data-target="#collapse1" area-expanded="false" aria-controls="collapse1" id="1">
-									<strong>Präpositionen</strong> <small>전치사</small>
+									<strong>E1. Präpositionen</strong> <small>전치사</small>
 								</button>
 							</h5>
 						</div>
@@ -66,7 +66,7 @@
 						<div class="card-header" id="heading2">
 							<h5 class="mb-0">
 								<button class="btn btn-outline-orange mt-1 mx-1 btn-block" type="button" data-toggle="collapse" data-target="#collapse2" area-expanded="false" aria-controls="collapse2" id="2">
-									<strong>Adverb im Satz</strong> <small>문장에서의 부사</small>
+									<strong>E2. Adverb im Satz</strong> <small>문장에서의 부사</small>
 								</button>
 							</h5>
 						</div>
@@ -114,7 +114,7 @@
 						<div class="card-header" id="heading3">
 							<h5 class="mb-0">
 								<button class="btn btn-outline-warning mt-1 mx-1 btn-block" type="button" data-toggle="collapse" data-target="#collapse3" area-expanded="false" aria-controls="collapse3" id="3">
-									<strong>Modalverben</strong> <small>화법조동사<br>화자의 주관적인 생각을 화법조동사를 사용하여 가능성, 필연성, 확신 등을 표현할 수 있다.</small>
+									<strong>E3. Modalverben</strong> <small>화법조동사<br>화자의 주관적인 생각을 화법조동사를 사용하여 가능성, 필연성, 확신 등을 표현할 수 있다.</small>
 								</button>
 							</h5>
 						</div>
@@ -204,7 +204,7 @@
 						<div class="card-header" id="heading4">
 							<h5 class="mb-0">
 								<button class="btn btn-outline-primary mt-1 mx-1 btn-block" type="button" data-toggle="collapse" data-target="#collapse4" area-expanded="false" aria-controls="collapse6" id="4">
-									<strong>Redewendungen</strong> <small>어휘와 숙어</small>
+									<strong>E4. Redewendungen</strong> <small>어휘와 숙어</small>
 								</button>
 							</h5>
 						</div>
@@ -212,126 +212,154 @@
 							<table class="table text-left bg-light">
 								<thead>
 									<tr>
+										<th class="text-center align-middle m-2 p-2" colspan="2"><button type="button" id="1" class="so btn btn-outline-dark btn-sm mr-1">▶</button></th>
+									</tr>
+									<tr>
 										<th scope="col" colspan="2">sich verabreden</th>
 									</tr>
 								</thead>
 								<tbody>
 									<tr>
-										<td width="50%">Hast du heute Abend / am ...... Zeit?</td>
-										<td>- Ja, klar.</td>
-									</tr>
-									<tr>
-										<td>&nbsp;</td>
-										<td>- Das weiß ich noch nicht.</td>
+										<td width="50%"><button type="button" id="2" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Hast du heute Abend / am ...... Zeit?</td>
+										<td><button type="button" id="3" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Ja, klar.</td>
 									</tr>
 									<tr>
 										<td>(Heute, morgen Abend / früh / Nachmittag)</td>
-										<td>- Vielleicht</td>
+										<td><button type="button" id="4" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Das weiß ich noch nicht.</td>
 									</tr>
 									<tr>
 										<td>&nbsp;</td>
-										<td>- Heute Abend / Am ..... habe ich leider keine Zeit.</td>
+										<td><button type="button" id="5" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Vielleicht</td>
+									</tr>
+									<tr>
+										<td>&nbsp;</td>
+										<td><button type="button" id="6" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Heute Abend / Am ..... habe ich leider keine Zeit.</td>
 									</tr>
 								</tbody>
 							</table>
 							<table class="table text-left">
 								<thead>
+									<tr>
+										<th class="text-center align-middle m-2 p-2" colspan="2"><button type="button" id="7" class="so btn btn-outline-dark btn-sm mr-1">▶</button></th>
+									</tr>
 									<tr>
 										<th scope="col" colspan="2">Einen Vorschlag machen und darauf reagieren</th>
 									</tr>
 								</thead>
 								<tbody>
 									<tr>
-										<td width="50%">Gehen wir ins Kino / ..... ?</td>
-										<td>- Gute Idee! / Okay!</td>
+										<td width="50%"><button type="button" id="8" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Gehen wir ins Kino / ..... ?</td>
+										<td><button type="button" id="9" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Gute Idee! / Okay!</td>
 									</tr>
 									<tr>
-										<td>Wollen wir heute Abend in den / in die / ins ..... gehen?</td>
-										<td>- Nein, leider nicht. Ich habe keine Zeit.</td>
+										<td><button type="button" id="10" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Wollen wir heute Abend in den / in die / ins ..... gehen?</td>
+										<td><button type="button" id="11" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Nein, leider nicht. Ich habe keine Zeit.</td>
 									</tr>
 									<tr>
-										<td>Hast du Lust auf Kino?</td>
-										<td>- Ja, gut!</td>
+										<td><button type="button" id="12" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Hast du Lust auf Kino?</td>
+										<td><button type="button" id="13" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Ja, gut!</td>
 									</tr>
 								</tbody>
 							</table>
 							<table class="table text-left bg-light">
 								<thead>
+									<tr>
+										<th class="text-center align-middle m-2 p-2" colspan="2"><button type="button" id="14" class="so btn btn-outline-dark btn-sm mr-1">▶</button></th>
+									</tr>
 									<tr>
 										<th scope="col" colspan="2">Anweisungen</th>
 									</tr>
 								</thead>
 								<tbody>
 									<tr>
-										<td width="50%">Hier dürfen Sie nicht rauchen.</td>
+										<td width="50%"><button type="button" id="15" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Das ist verboten.</td>
 										<td>&nbsp;</td>
 									</tr>
 									<tr>
-										<td>Das ist verboten.</td>
-										<td>&nbsp;</td>
+										<td><button type="button" id="16" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Hier dürfen Sie nicht rauchen.</td>
+										<td><button type="button" id="17" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Warum nicht?</td>
 									</tr>
 									<tr>
-										<td colspan="2">Hier kann man Bücher lesen.</td>
+										<td><button type="button" id="18" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Das ist verboten.</td>
+										<td><button type="button" id="19" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Na, gut. Dann höre ich auf.</td>
 									</tr>
 									<tr>
-										<td colspan="2">Hier muss man leise sprechen.</td>
+										<td colspan="2"><button type="button" id="20" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Hier kann man Bücher lesen.</td>
 									</tr>
 									<tr>
-										<td style="max-height: 10px; font-size: 1pt;" class="p-0 m-0" colspan="2">&nbsp;</td>
+										<td colspan="2"><button type="button" id="21" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Hier muss man leise sprechen.</td>
 									</tr>
 									<tr>
-										<td colspan="2">Dann solltest du in die Disko gehen.</td>
+										<td colspan="2"><button type="button" id="22" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Hier darf man nicht rauchen.</td>
 									</tr>
 									<tr>
-										<td colspan="2">Dann sollte sie ins Kino gehen.</td>
+										<td><button type="button" id="23" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Ich möchte tanzen.</td>
+										<td><button type="button" id="24" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Dann solltest du in die Disko gehen.</td>
+									</tr>
+									<tr>
+										<td><button type="button" id="25" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Sie möchte einen Film sehen.</td>
+										<td><button type="button" id="26" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Dann sollte sie ins Kino gehen.</td>
 									</tr>
 								</tbody>
 							</table>
 							<table class="table text-left">
 								<thead>
 									<tr>
-										<th scope="col" colspan="2">Verabredung zusagen</th>
+										<th class="text-center align-middle m-2 p-2" colspan="2"><button type="button" id="27" class="so btn btn-outline-dark btn-sm mr-1">▶</button></th>
 									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<td colspan="2">Ja, klar.</td>
-									</tr>
-									<tr>
-										<td colspan="2">Gute Idee! / Okay!</td>
-									</tr>
-									<tr>
-										<td colspan="2">Ja, gut!</td>
-									</tr>
-								</tbody>
-							</table>
-							<table class="table text-left bg-light">
-								<thead>
 									<tr>
 										<th scope="col" colspan="2">Verabredung absagen</th>
 									</tr>
 								</thead>
 								<tbody>
 									<tr>
-										<td colspan="2">Heute Abend / Am …… habe ich leider keine Zeit.</td>
+										<td colspan="2"><button type="button" id="28" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Ich kann leider nicht.</td>
 									</tr>
 									<tr>
-										<td colspan="2">Nein, leider nicht. Ich habe keine Zeit.</td>
+										<td colspan="2"><button type="button" id="29" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Das geht leider nicht.</td>
+									</tr>
+									<tr>
+										<td colspan="2"><button type="button" id="30" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Das passt leider nicht.</td>
+									</tr>
+								</tbody>
+							</table>
+							<table class="table text-left bg-light">
+								<thead>
+									<tr>
+										<th class="text-center align-middle m-2 p-2" colspan="2"><button type="button" id="31" class="so btn btn-outline-dark btn-sm mr-1">▶</button></th>
+									</tr>
+									<tr>
+										<th scope="col" colspan="2">Einen Zeitpunkt angeben</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td width="50%"><button type="button" id="32" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Wann / Um wie viel Uhr?</td>
+										<td><button type="button" id="33" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Am Dienstag / Abend / Mittwochabend /.....</td>
+									</tr>
+									<tr>
+										<td>&nbsp;</td>
+										<td><button type="button" id="34" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Um ..... Uhr.</td>
+									</tr>
+									<tr>
+										<td>&nbsp;</td>
+										<td><button type="button" id="35" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Um drei / halb vier.</td>
 									</tr>
 								</tbody>
 							</table>
 							<table class="table text-left">
 								<thead>
 									<tr>
-										<th scope="col" colspan="2">Unsicherheit ausdrücken </th>
+										<th class="text-center align-middle m-2 p-2" colspan="2"><button type="button" id="36" class="so btn btn-outline-dark btn-sm mr-1">▶</button></th>
+									</tr>
+									<tr>
+										<th scope="col" colspan="2">Uhrzeit</th>
 									</tr>
 								</thead>
 								<tbody>
 									<tr>
-										<td colspan="2">Das weiß ich noch nicht.</td>
-									</tr>
-									<tr>
-										<td colspan="2">Vielleicht.</td>
+										<td width="50%"><button type="button" id="37" class="so btn btn-outline-dark btn-sm mr-1">▶</button>Wie spät ist es? / Wie viel Uhr ist es?</td>
+										<td><button type="button" id="38" class="so btn btn-outline-dark btn-sm mr-1">▶</button>- Es ist Viertel vor drei / halb sechs.</td>
 									</tr>
 								</tbody>
 							</table>
@@ -343,12 +371,127 @@
 	</div>
 </section>
 
-		
+<div id="last" class="d-none"></div>
+
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="./js/jquery-3.4.1.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="./js/popper.min.js"></script>
 		<script src="./js/bootstrap.js"></script>
+		<script src="./js/howler.core.js"></script>
+		<script>
+			// 소리 출력 전역 변수와 함수
+			var sen = new Array(), pa = new Array(), he = new Array(), last;
+			$(".so").each(function() {
+				var t = $(this);
+				var ti = t.attr("id");
+				sen[ti] = 0;
+				pa[ti] = t.html();
+				he[ti] = t.height();
+			});
+
+			function stopAll() {
+				$(".so").each(function() {
+					$(this).html(pa[$(this).attr("id")]);
+				});
+			}
+
+			var nagehts = new Howl({
+				// src: ["./sounds/Reihe 1/r1 H.mp3"],
+				src: ["./sounds/Reihe 7/r7 E.mp3"],
+				sprite : {
+					"1": [4094, 28755],
+					"2": [7098, 8476],
+					"3": [15884, 1873],
+					"4": [18059, 2349],
+					"5": [20493, 1993],
+					"6": [22632, 10384],
+					"7": [34611, 30852],
+					"8": [39848, 2293],
+					"9": [42883, 3213],
+					"10": [46869, 7642],
+					"11": [54812, 4374],
+					"12": [60619, 2610],
+					"13": [63591, 1935],
+					"14": [66744, 44946],
+					"15": [69775, 2616],
+					"16": [73261, 2320],
+					"17": [76517, 1599],
+					"18": [79092, 1983],
+					"19": [82209, 2629],
+					"20": [85993, 2656],
+					"21": [89520, 2884],
+					"22": [93200, 3038],
+					"23": [97480, 1967],
+					"24": [100733, 2678],
+					"25": [104701, 2710],
+					"26": [108598, 2550],
+					"27": [112828, 13137],
+					"28": [116575, 1890],
+					"29": [119600, 2094],
+					"30": [123622, 2236],
+					"31": [127654, 19274],
+					"32": [131635, 2425],
+					"33": [135783, 5348],
+					"34": [141504, 2157],
+					"35": [144232, 2634],
+					"36": [148789, 14147],
+					"37": [151410, 5134],
+					"38": [157753, 5186]
+				},
+				html5: true,
+				volume: 1,
+				format: "mp3",
+				preload: true,
+				onloaderror: function() {
+					$(".alert").append("<br /><strong class=\"font-weight-bold text-dark display-4\">페이지를 다시 읽어주시기 바래요.</strong>");
+					console.log("다시 읽어주세요!");
+				},
+				onload: function() {
+					// 음성 준비되면 HV 버튼 나타내기 
+					$("#whole").show();
+					$("#alt").hide();
+					// $(".alert").hide();
+
+					$(".so").on("click", function(idx) {
+							var t = $(this);
+							var ti = t.attr("id");
+
+						if($("div#last").text() == "" || t.text() == "❚❚") {
+							$("#last").text(ti);
+							t.text("■");
+							nagehts.seek();
+							nagehts.play(ti);
+							sen[ti]++;
+				
+							last = ti;
+				
+							$("#cnt-"+ti).text(sen[ti]);
+						} else if(last == ti && nagehts.playing($("div#last").text())) {
+							$("#last").text("");
+							t.html(pa[ti]);
+							nagehts.pause();
+							sen[ti]--;
+							$("#cnt-"+ti).text(sen[ti]);
+						}
+
+					});
+				},
+				onend: function() {
+					$("div#last").text("");
+					stopAll();
+					$("#cnt-"+last).text(sen[last]);
+					if(sen[last] == 2) {
+						$("#"+last).find(".tran").show();
+					}
+				}
+
+
+			});
+
+
+	
+	</script>
 <? } ?>
 <?php include "footer.php"; ?>
 	</body>

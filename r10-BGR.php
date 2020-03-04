6ef510dd50e150c07bb6ceda509f6476a9d390db
cansmile <cansmile@gmail.com>
2020-03-05 08:19:27 +0900

    R10 BGR 수정 반영
    번역 문제로 변경

--- r10-BGR.php
+++ r10-BGR.php
@@ -37,9 +37,16 @@
 					<table class="table">
 						<tbody>
 							<tr>
-								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-2.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
-								<td class="border-0"><small>(공간) ~위로, 위에</small><br>Die Mutter setzt das Kind auf den Stuhl.
-								<span class="tran"><small><br>어머니는 아이를 의자에 앉힌다.</small></span></td>
+								<td class="border-0 text-center" width="120" rowspan="2"><img src="./images/Reihe 10/Reihe-10-BGR-2.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~위로, 위에</small>
+								</td>
+							</tr>
+							<tr>
+								<td class="border-0" style="line-height: 250%;">
+									<div class="input-group">
+										Die Mutter setzt das Kind <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 100px;" aria-label="." id="qst-1">den Stuhl.
+									</div>
+									<span class="tran"><small><br>어머니는 아이를 의자에 앉힌다.</small></span>
 							</tr>
 						</tbody>
 					</table>
@@ -48,9 +55,16 @@
 					<table class="table">
 						<tbody>
 							<tr>
-								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-3.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
-								<td class="border-0"><small>(공간) ~안으로, 안에</small><br>Ich stecke den Schlüssel ins Schloß.
-								<span class="tran"><small><br>나는 이번 휴가에 바다에 간다.</small></span></td>
+								<td class="border-0 text-center" width="120" rowspan="2"><img src="./images/Reihe 10/Reihe-10-BGR-3.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~안으로, 안에</small>
+								</td>
+							</tr>
+							<tr>
+								<td class="border-0" style="line-height: 250%;">
+									<div class="input-group">
+										Ich stecke den Schlüssel <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 100px;" aria-label="." id="qst-2">Schloß.
+									</div>
+									<span class="tran"><small><br>나는 이번 휴가에 바다에 간다.</small></span>
 							</tr>
 						</tbody>
 					</table>
@@ -59,9 +73,16 @@
 					<table class="table">
 						<tbody>
 							<tr>
-								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-4.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
-								<td class="border-0"><small>(공간) ~가로, 가에, ~측면에</small><br>Ich fahre in diesem Urlaub ans Meer.
-								<span class="tran"><small><br>나는 나무 앞에 탁자를 놓는다.</small></span></td>
+								<td class="border-0 text-center" width="120" rowspan="2"><img src="./images/Reihe 10/Reihe-10-BGR-4.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~가로, 가에, ~측면에</small>
+								</td>
+							</tr>
+							<tr>
+								<td class="border-0" style="line-height: 250%;">
+									<div class="input-group">
+										Ich fahre in diesem Urlaub <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 100px;" aria-label="." id="qst-3">Meer.
+									</div>
+									<span class="tran"><small><br>나는 나무 앞에 탁자를 놓는다.</small></span>
 							</tr>
 						</tbody>
 					</table>
@@ -70,9 +91,16 @@
 					<table class="table">
 						<tbody>
 							<tr>
-								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-5.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
-								<td class="border-0"><small>(공간) ~뒤로, 뒤에</small><br>Ich lege die Kamera hinter den Baum.
-								<span class="tran"><small><br>나는 집 옆으로 차를 몰고 간다.</small></span></td>
+								<td class="border-0 text-center" width="120" rowspan="2"><img src="./images/Reihe 10/Reihe-10-BGR-5.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~뒤로, 뒤에</small>
+								</td>
+							</tr>
+							<tr>
+								<td class="border-0" style="line-height: 250%;">
+									<div class="input-group">
+										Ich lege die Kamera <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 100px;" aria-label="." id="qst-4">den Baum.
+									</div>
+									<span class="tran"><small><br>나는 집 옆으로 차를 몰고 간다.</small></span>
 							</tr>
 						</tbody>
 					</table>
@@ -81,9 +109,16 @@
 					<table class="table">
 						<tbody>
 							<tr>
-								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-6.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
-								<td class="border-0"><small>(공간) ~앞으로, 앞에</small><br>Ich stelle den Tisch vor den Baum.
-								<span class="tran"><small><br>나는 탁자 밑에 책을 놓는다.</small></span></td>
+								<td class="border-0 text-center" width="120" rowspan="2"><img src="./images/Reihe 10/Reihe-10-BGR-6.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~앞으로, 앞에</small>
+								</td>
+							</tr>
+							<tr>
+								<td class="border-0" style="line-height: 250%;">
+									<div class="input-group">
+										Ich stelle den Tisch <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 100px;" aria-label="." id="qst-5">den Baum.
+									</div>
+									<span class="tran"><small><br>나는 탁자 밑에 책을 놓는다.</small></span>
 							</tr>
 						</tbody>
 					</table>
@@ -92,9 +127,16 @@
 					<table class="table">
 						<tbody>
 							<tr>
-								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-7.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
-								<td class="border-0"><small>(공간) ~위로, 위에</small><br>Ich wohne über ihm.
-								<span class="tran"><small><br>나는 열쇠를 자물쇠에 꽂는다.</small></span></td>
+								<td class="border-0 text-center" width="120" rowspan="2"><img src="./images/Reihe 10/Reihe-10-BGR-7.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~위로, 위에</small>
+								</td>
+							</tr>
+							<tr>
+								<td class="border-0" style="line-height: 250%;">
+									<div class="input-group">
+										Ich wohne <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 100px;" aria-label="." id="qst-6">ihm.
+									</div>
+									<span class="tran"><small><br>나는 열쇠를 자물쇠에 꽂는다.</small></span>
 							</tr>
 						</tbody>
 					</table>
@@ -103,9 +145,16 @@
 					<table class="table">
 						<tbody>
 							<tr>
-								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-8.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
-								<td class="border-0"><small>(공간) ~옆으로, 옆에</small><br>lch fahre das Auto neben das Haus.
-								<span class="tran"><small><br>나는 카메라를 나무 뒤에 놓는다.</small></span></td>
+								<td class="border-0 text-center" width="120" rowspan="2"><img src="./images/Reihe 10/Reihe-10-BGR-8.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~옆으로, 옆에</small>
+								</td>
+							</tr>
+							<tr>
+								<td class="border-0" style="line-height: 250%;">
+									<div class="input-group">
+										lch fahre das Auto <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 100px;" aria-label="." id="qst-7">das Haus.
+									</div>
+									<span class="tran"><small><br>나는 카메라를 나무 뒤에 놓는다.</small></span>
 							</tr>
 						</tbody>
 					</table>
@@ -114,9 +163,16 @@
 					<table class="table">
 						<tbody>
 							<tr>
-								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-9.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
-								<td class="border-0"><small>(공간) ~사이로, 사이에</small><br>Ich lege den Hut zwischen den Tisch und den Baum.
-								<span class="tran"><small><br>나는 그 남자 위에 살고 있다.</small></span></td>
+								<td class="border-0 text-center" width="120" rowspan="2"><img src="./images/Reihe 10/Reihe-10-BGR-9.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~사이로, 사이에</small>
+								</td>
+							</tr>
+							<tr>
+								<td class="border-0" style="line-height: 250%;">
+									<div class="input-group">
+										Ich lege den Hut <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 100px;" aria-label="." id="qst-8">den Tisch und den Baum.
+									</div>
+									<span class="tran"><small><br>나는 그 남자 위에 살고 있다.</small></span>
 							</tr>
 						</tbody>
 					</table>
@@ -125,14 +181,28 @@
 					<table class="table">
 						<tbody>
 							<tr>
-								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-10.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
-								<td class="border-0"><small>(공간) ~아래로, 아래에</small><br>Ich lege das Buch unter den Tisch.
-									<span class="tran"><small><br>나는 탁자와 나무 사이에 모자를 놓는다.</small></span></td>
+								<td class="border-0 text-center" width="120" rowspan="2"><img src="./images/Reihe 10/Reihe-10-BGR-10.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~아래로, 아래에</small>
+								</td>
+							</tr>
+							<tr>
+								<td class="border-0" style="line-height: 250%;">
+									<div class="input-group">
+										Ich lege das Buch <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 100px;" aria-label="." id="qst-9">den Tisch.
+									</div>
+									<span class="tran"><small><br>나는 탁자와 나무 사이에 모자를 놓는다.</small></span>
 							</tr>
 						</tbody>
 					</table>
 				</div>
 			</div>
+			<!-- 정답화인 버튼 시작 -->
+			<div class="row">
+				<div class="btn my-3 btn-light col-sm-12 col-md-12 col-lg-12" id="chk">
+					정답확인
+				</div>
+			</div>
+			<!-- 정답확인 버튼 끝 -->
 			<div class="row">
 				<div class="col-lg-12 mb-4 mt-2 text-center">
 					<h2> Abkürzungen <br>
@@ -182,6 +252,243 @@
 	<script src="./js/jquery-3.4.1.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="./js/bootstrap.js"></script>
+	<!-- interact.min.js -->
+	<script src="./js/ion.sound.min.js"></script>
+	<script>
+		$("#0").hide();
+		$("#0_p").hide();
+
+		$(".tran").hide();
+		$(".ant").hide();
+		var an=new Array();
+		var an=["auf","ins","ans","hinter","vor","über","neben","zwischen","unter"];
+		$(document).ready(function() {
+			// 값 확인해보자, io값이 참이면 전체 검사
+			function rfchk(th, io) {
+				var q, qn, a, b, fl;
+				q=th.val().length;
+				qn=(th.attr("id").substr(4))-1;
+				a=th.val();
+				a=a.replace(/ /gi, "");
+				if( !$.isArray(an[qn])) {
+					// 1 인 경우
+					if(io) {
+						b=an[qn];
+					}
+					else {
+						b=an[qn].substr(0, q);
+					}
+					b=b.replace(/ /gi, "");
+					if(a==b) {
+						return true;
+					}
+				}
+				else {
+					// 2 이상인 경우
+					for(var fd=0;
+					fd < an[qn].length;
+					fd++) {
+						if(io) {
+							b=an[qn][fd];
+						}
+						else {
+							b=an[qn][fd].substr(0, q);
+						}
+						b=b.replace(/ /gi, "");
+						if(a==b) {
+							return true;
+						}
+					}
+				}
+			}
+
+			$(".q").on("keyup", function () {
+				$(this).removeClass("bg-danger");
+				$(this).removeClass("bg-success");
+				$("#ant-"+$(this).attr("id").substr(4)).removeClass("text-danger");
+				$("#ant-"+$(this).attr("id").substr(4)).removeClass("text-success");
+				if(rfchk($(this))) {
+					$(this).addClass("text-white font-weight-bold");
+					$(this).addClass("bg-success");
+					$("#ant-"+$(this).attr("id").substr(4)).addClass("text-success");
+				}
+				else {
+					$(this).addClass("text-white font-weight-bold");
+					$(this).addClass("bg-danger");
+					$("#ant-"+$(this).attr("id").substr(4)).addClass("text-danger");
+				}
+				if( !$(this).val()) {
+					$(this).removeClass("bg-danger");
+					$(this).removeClass("bg-success");
+					$(this).removeClass("text-white font-weight-bold");
+				}
+				if($(this).val()) {
+					$("#ant-"+$(this).attr("id").substr(4)).show();
+					$("#ant-"+$(this).attr("id").substr(4)).text($(this).val());
+				}
+				else {
+					$("#ant-"+$(this).attr("id").substr(4)).hide();
+				}
+			}
+			);
+			$(".q").on("focusin", function() {
+				$("#ant-"+$(this).attr("id").substr(4)).show();
+				if( !$("#ant-"+$(this).attr("id").substr(4)).text()) {
+					$("#ant-"+$(this).attr("id").substr(4)).text($(this).val());
+				}
+				if($("#ant-"+$(this).attr("id").substr(4)).text()) {
+					if(rfchk($(this))) {
+						$(this).addClass("text-white font-weight-bold");
+						$(this).addClass("bg-success");
+						$("#ant-"+$(this).attr("id").substr(4)).addClass("text-success");
+					}
+					else {
+						$(this).addClass("text-white font-weight-bold");
+						$(this).addClass("bg-danger");
+						$("#ant-"+$(this).attr("id").substr(4)).addClass("text-danger");
+					}
+				}
+			}
+			);
+			$(".q").on("focusout", function() {
+				$("#ant-"+$(this).attr("id").substr(4)).hide();
+				if(rfchk($(this), true)) {
+					$(this).addClass("bg-success");
+					$(this).addClass("text-white");
+				}
+				else {
+					$(this).addClass("bg-danger");
+				}
+				if($(this).val()) {
+					if($(this).hasClass("bg-danger")) {
+						ion.sound.play("Cartoon_Boing");
+					}
+					else if($(this).hasClass("bg-success")) {
+						ion.sound.play("dingdongdang");
+						$(this).prop("disabled", true);
+					}
+				}
+				$(this).removeClass("bg-danger");
+				if( !$(this).attr("disabled")) {
+					$(this).removeClass("text-white font-weight-bold");
+					$(this).removeClass("bg-success");
+				}
+			}
+			)
+			/* 입력하는 문자 확인(정답 표시 없음) 여기까지 */
+
+			$("#chk").on("click", function() {
+				var na="";
+				var ri=0;
+				var qst=$(".q").length;
+				$(".q").each(function () {
+					if(na !="") {
+						na +=", ";
+					}
+					if($(this).val()=="") {
+						na +=$(this).attr("id").substr(4, 1);
+					}
+				}
+				);
+				if($(this).attr("id")=="done") {}
+				else if(na=="") {
+					for(var i=0;
+					i < an.length;
+					i++) {
+						var oran=$("#qst-"+(i+1)).val();
+						if(rfchk($("#qst-"+(i+1)), true)) {
+							$("#qst-"+(i+1)).addClass("bg-success text-white rounded font-weight-bold p-1 px-2");
+							$("#qst-"+(i+1)).removeClass("rounded-0");
+						}
+						else {
+							$("#qst-"+(i+1)).val(oran);
+							$("#qst-"+(i+1)).attr("disabled", true);
+							$("#qst-"+(i+1)).addClass("bg-danger text-white rounded font-weight-bold p-1 px-2 ml-1");
+							$("#qst-"+(i+1)).removeClass("rounded-0");
+
+							if( !$.isArray(an[i])) {
+								$("#qst-"+(i+1)).after("<div class=\"d-block text-dark bg-warning rounded p-1 m-1 px-2 font-weight-bold\" style=\"position: relative; top: -6px; max-width: 100px; min-width: 100px;\">"+an[i]+"</div>");}
+							else {
+								// 2 이상인 경우
+								var r = "<div class=\"d-block text-dark bg-warning rounded p-1 m-1 px-2 font-weight-bold\" style=\"position: relative; top: -6px; max-width: 100px; min-width: 100px;\">";
+								for(var fd = (an[i].length-1);
+								fd >= 0;
+								fd--) {
+									if(fd < (an[i].length-1)) {
+										r = r + " / ";
+									}
+									r = r + an[i][fd];
+								}
+								r = r +"</div>";
+								if($("span.sen").length > 0) {
+									$("#qst-"+(i+1)).closest("span.sen").after(r);
+								} else {
+									$("#qst-"+(i+1)).after(r);
+								}
+							}
+
+						}
+						if($("#qst-"+(i+1)).hasClass("bg-success")) {
+							ri++;
+						}
+					}
+					// 정답 확인 div 상자 배경색 속성 없애기
+					$(this).removeClass("btn-light ");
+
+					var qa = $(".q").length; // 전체 문항 수
+					var qr = $(".bg-success").length; // 맞춘 항목 수
+					var pe = (qr / qa) * 100; // 정답 비율
+					var tcl = "white"; // 기본 문자색
+
+					// 분류 기준은 100%, 80%, 60%, 40%
+					if(pe > 99) {
+						var st = "원어민이세요?";
+						var cl = "lime";
+						var tcl = "dark";
+					} else if(pe > 74) {
+						var st = "어! 좀 하시는데요~^^";
+						var cl = "success";
+					} else if(pe > 49) {
+						var st = "쓰읍~ 다시 해 보실까요!";
+						var cl = "primary";
+					} else {
+						var st = "좀 더 분발해 주세요";
+						var cl = "danger";
+					}
+
+					$(this).addClass("btn-" + cl + " text-" + tcl);
+					$(this).html("<h4>" + qa + "문제 중 " + qr + "개를 맞히셨네요!<br>" + st + "</h4>");
+					$(this).prop("disabled", true);
+					$(".tran").show();
+					$(this).attr("id", "done");
+				}
+				else {
+					alert("모든 문제를 풀어주세요!");
+					// alert(na+"번 문제를 풀어주세요!");
+				}
+				;
+			}
+			);
+
+			var pan=new Array();
+			// pan=[1,2,3,4,5,6,7,8,9,10];
+			pan = [];
+			for(var p=0;
+			p < pan.length;
+			p++) {
+				var pann="#qst-"+pan[p];
+				$(pann).val(an[(pan[p]-1)]);
+				$(pann).addClass("bg-success text-white font-weight-bold");
+				$(pann).prop("disabled", true);
+				// $(pann).closest("tr").find(".tran").show();
+			}			
+
+		}
+		);
+
+	</script>
+	<!-- ion.sound finished -->
+
 	<? } ?>
 	<?php include "footer.php"; ?>
 </body>

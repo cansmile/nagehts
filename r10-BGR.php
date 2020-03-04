619899e560bbd067d314961b0804bff8b402610a
cansmile <cansmile@gmail.com>
2020-03-05 07:41:26 +0900

    R10 BGR 반영
    기능 없는 페이지

---
+++ r10-BGR.php
@@ -0,0 +1,179 @@
+<?php include "header.php"; ?>
+<body>
+	<?php include "nav.php"; ?>
+	<?php if(ul()) { ?>
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0">독일어에서 3, 4격 지배 전치사는 장소의 의미로 사용될 때, 정지 상태를 나타낼 때는 3격을 취하고, 장소의 이동이나 운동의 방향을 나타낼 때는 4격을 지배한다.</td>
+							</tr>
+							<tr>
+								<td class="border-0 text-center"><strong>an, auf, hinter, in, neben, unter, vor, über, zwischen</strong></td>
+							</tr>
+							<tr>
+								<td class="border-0 text-center">
+									<div class="border border-dark rounded p-2 mx-auto">
+										<img src="./images/Reihe 10/Reihe-10-BGR-1.png" alt="Wechselpräpositionen" style="max-width: 100%; height: auto;"><br>
+										Der Hund geht zur Katze. Er sitzt neben ihr.<br><small>그 강아지가 고양이에게 간다. 강아지는 고양이 곁에 앉아 있다.</small>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12 mb-4 mt-2 text-center">
+					<h2> Präpositionmen des Ortes <br>
+					<small>장소와 연관된 3·4격 지배전치사</small>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-2.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~위로, 위에</small><br>Die Mutter setzt das Kind auf den Stuhl.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-3.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~안으로, 안에</small><br>Ich stecke den Schlüssel ins Schloß.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-4.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~가로, 가에, ~측면에</small><br>Ich fahre in diesem Urlaub ans Meer.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-5.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~뒤로, 뒤에</small><br>Ich lege die Kamera hinter den Baum.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-6.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~앞으로, 앞에</small><br>Ich stelle den Tisch vor den Baum.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-7.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~위로, 위에</small><br>Ich wohne über ihm.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-8.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~옆으로, 옆에</small><br>lch fahre das Auto neben das Haus.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-9.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~사이로, 사이에</small><br>Ich lege den Hut zwischen den Tisch und den Baum.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td class="border-0 text-center" width="120"><img src="./images/Reihe 10/Reihe-10-BGR-10.png" alt="Präpositionmen des Ortes" style="max-width: 100%; height: auto;"></td>
+								<td class="border-0"><small>(공간) ~아래로, 아래에</small><br>Ich lege das Buch unter den Tisch.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12 mb-4 mt-2 text-center">
+					<h2> Abkürzungen <br>
+					<small>축약형</small>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<table class="table table-striped text-center">
+						<tbody>
+							<tr>
+								<td class="border-0 align-middle">m / n + Dativ</td>
+								<td class="border-0 align-middle">in<br>an<br>von<br>bei<br>zu</td>
+								<td class="border-0 align-middle">+</td>
+								<td class="border-0 align-middle">dem</td>
+								<td class="border-0 align-middle">=</td>
+								<td class="border-0 align-middle">im<br>am<br>vom<br>beim<br>zum</td>
+							</tr>
+							<tr>
+								<td class="border-0 align-middle">f +  Dativ</td>
+								<td class="border-0 align-middle">zu</td>
+								<td class="border-0 align-middle">+</td>
+								<td class="border-0 align-middle">der</td>
+								<td class="border-0 align-middle">=</td>
+								<td class="border-0 align-middle">zur</td>
+							</tr>
+							<tr>
+								<td class="border-0 align-middle">n + Akkusativ</td>
+								<td class="border-0 align-middle">in<br>an</td>
+								<td class="border-0 align-middle">+</td>
+								<td class="border-0 align-middle">das</td>
+								<td class="border-0 align-middle">=</td>
+								<td class="border-0 align-middle">ins<br>ans</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 border border-dark p-5 rounded text-center">그러나 문장에서 강조의 의미로 사용될 때는 축약형이 사용되지 않는다.<br><br>Ich gehe jetzt in das Kino, das du mir empfohlen hast.<br><small>나는 네가 나에게 추천했던 그 영화관으로 지금 간다.</small></div>
+			</div>
+		</div>
+	</section>
+	
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="./js/jquery-3.4.1.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="./js/bootstrap.js"></script>
+	<? } ?>
+	<?php include "footer.php"; ?>
+</body>
+</html>

61aa3f992234361a17f84f0a2d92eb04eb8e684c
cansmile <cansmile@gmail.com>
2020-03-05 16:15:17 +0900

    R10 C1 반영
    선택지 문제 유형으로 등록

---
+++ r10-C1.php
@@ -0,0 +1,259 @@
+<?php include "header.php"; ?>
+
+<body>
+	<?php include "nav.php"; ?>
+	<?php if(ul()) { ?>
+	<!-- 보기시작 -->
+	<section class="bg-white rounded p-2" style="position: fixed; bottom: 0; z-index: 9999; width: 100%;" id="wahl">
+		<div class="container">
+			<div class="row">
+				<div class="col display-4 bg-<?php echo($color); ?> rounded text-center text-white font-weight-bold col-12">Wahl<small>선택지</small></div>
+				<div class="col-12" id="itms">
+					<button type="button" class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm" id="1">
+						Deutsches Hygienemuseum
+						<span class="tran"><br><small>독일 위생박물과</small></span>
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans6 btn-lg btn-outline-dark itm" id="2">
+						Frauenkirche
+						<span class="tran"><br><small>프라우엔 교회</small></span>
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm" id="3">
+						Semperoper
+						<span class="tran"><br><small>젬퍼오퍼</small></span>
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="4">
+						Sächsische Schwei
+						<span class="tran"><br><small>작센 스위스</small></span>
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm" id="5">
+						Volkswagen Gläserne Manufaktu
+						<span class="tran"><br><small>폭스바겐 글레레너 마누팍투</small></span>
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="6">
+						Zwinger
+						<span class="tran"><br><small>츠빙어</small></span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<!-- 고르는 아이템들 -->
+			<div class="row">
+				<div class="col-lg-12 mb-4 mt-2 text-center">
+					<h3>[ <small>단어를 선택하고 알맞은 위치의 노란 단추를 누르세요.</small> ]</h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-3">
+					<table class="table border-0">
+						<tbody>
+							<tr>
+								<td height="250" class="border-0 align-middle"><img src="./images/Reihe 10/Reihe-10-C1-1.png" alt="Welche Fotos und Texte passen zusammen?" style="max-width: 100%; height: auto;"></td>
+							</tr>
+							<tr>
+								<td class="border-0">
+									<div class="itm-lst 1itm" id="lst-1">
+										<h2 class="btn btn-warning btn-xl ttl d-block"> ▼ </h2>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-3">
+					<table class="table border-0">
+						<tbody>
+							<tr>
+								<td height="250" class="border-0 align-middle"><img src="./images/Reihe 10/Reihe-10-C1-2.png" alt="Welche Fotos und Texte passen zusammen?" style="max-width: 100%; height: auto;"></td>
+							</tr>
+							<tr>
+								<td class="border-0">
+									<div class="itm-lst 1itm" id="lst-2">
+										<h2 class="btn btn-warning btn-xl ttl d-block"> ▼ </h2>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-3">
+					<table class="table border-0">
+						<tbody>
+							<tr>
+								<td height="250" class="border-0 align-middle"><img src="./images/Reihe 10/Reihe-10-C1-3.png" alt="Welche Fotos und Texte passen zusammen?" style="max-width: 100%; height: auto;"></td>
+							</tr>
+							<tr>
+								<td class="border-0">
+									<div class="itm-lst 1itm" id="lst-3">
+										<h2 class="btn btn-warning btn-xl ttl d-block"> ▼ </h2>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-3">
+					<table class="table border-0">
+						<tbody>
+							<tr>
+								<td height="250" class="border-0 align-middle"><img src="./images/Reihe 10/Reihe-10-C1-4.png" alt="Welche Fotos und Texte passen zusammen?" style="max-width: 100%; height: auto;"></td>
+							</tr>
+							<tr>
+								<td class="border-0">
+									<div class="itm-lst 1itm" id="lst-4">
+										<h2 class="btn btn-warning btn-xl ttl d-block"> ▼ </h2>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-3">
+					<table class="table border-0">
+						<tbody>
+							<tr>
+								<td height="250" class="border-0 align-middle"><img src="./images/Reihe 10/Reihe-10-C1-5.png" alt="Welche Fotos und Texte passen zusammen?" style="max-width: 100%; height: auto;"></td>
+							</tr>
+							<tr>
+								<td class="border-0">
+									<div class="itm-lst 1itm" id="lst-5">
+										<h2 class="btn btn-warning btn-xl ttl d-block"> ▼ </h2>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-3">
+					<table class="table border-0">
+						<tbody>
+							<tr>
+								<td height="250" class="border-0 align-middle"><img src="./images/Reihe 10/Reihe-10-C1-6.png" alt="Welche Fotos und Texte passen zusammen?" style="max-width: 100%; height: auto;"></td>
+							</tr>
+							<tr>
+								<td class="border-0">
+									<div class="itm-lst 1itm" id="lst-6">
+										<h2 class="btn btn-warning btn-xl ttl d-block"> ▼ </h2>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<!-- 정답화인 버튼 시작 -->
+			<div class="row">
+				<div class="btn my-3 btn-light col-sm-12 col-md-12 col-lg-12" id="chk"> 정답확인 </div>
+			</div>
+			<!-- 정답확인 버튼 끝 -->
+		</div>
+	</section>
+
+	<div id="marg"></div>
+
+		
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="./js/jquery-3.4.1.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="./js/popper.min.js"></script>
+	<script src="./js/bootstrap.js"></script>
+	<script src="./js/taptogroup.js"></script>
+	<!-- interact.min.js -->
+	<script src="./js/ion.sound.min.js"></script>
+	<script>
+		$("#0").hide();
+		$("#0_p").hide();
+		$(".tran").hide();
+		$("#chk").hide();
+		$(document).ready(function() {
+					// 정답확인
+					$("#chk").on("click", function() {
+						var na="";
+						if($("#itms").find("button").length < 1) {
+							$(".tran").show();
+
+						// 정답 확인 div 상자 배경색 속성 없애기
+						$(this).removeClass("btn-light ");
+
+						$(".itm-lst").each(function() {
+							if($(this).find(".btn")) {
+								$(this).find(".btn").addClass("text-success");
+							}
+						});
+
+						var qa = $(".itm-lst").length; // 전체 문항 수
+						var qr = $(".text-success").length; // 맞춘 항목 수
+						var pe = (qr / qa) * 100; // 정답 비율
+						var tcl = "white"; // 기본 문자색
+
+						// 분류 기준은 100%, 80%, 60%, 40%
+						if(pe > 99) {
+							var st = "원어민이세요?";
+							var cl = "lime";
+							var tcl = "dark";
+						} else if(pe > 74) {
+							var st = "어! 좀 하시는데요~^^";
+							var cl = "success";
+						} else if(pe > 49) {
+							var st = "쓰읍~ 다시 해 보실까요!";
+							var cl = "primary";
+						} else {
+							var st = "좀 더 분발해 주세요";
+							var cl = "danger";
+						}
+
+						$(this).addClass("btn-" + cl + " text-" + tcl);
+						$(this).html("<h4>" + qa + "문제 중 " + qr + "개를 맞히셨네요!<br>" + st + "</h4>");
+
+							$(".btn-lg").text().appendTo($(this).closest("td"));
+							$(".btn-lg").remove();
+						}
+						else {
+							$("div.itm-lst").each(function(idx) {
+								if( !$(this).find("button").length) {
+									if(na !="") {
+										na +=", ";
+									}
+									na +=(idx+1);
+								}
+							}
+							);
+							alert("모든 문제를 풀어주세요!");
+							// alert(na+"번 문제를 풀어주세요!");
+						}
+					}
+					);
+					$("#0").show();
+					$(".alert").hide();
+
+					<?php include "wahl.php"; ?>
+
+					var pan=new Array();
+					// pan = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14"];
+					pan=[1];
+					var il=$("#itms>.itm").length;
+					for(var p=0;
+					p < pan.length;
+					p++) {
+						var pani="#lst-"+ pan[p];
+						$(".itm").each(function() {
+							if($(this).hasClass("ans"+ pan[p])) {
+								$("#"+ $(this).attr("id")).appendTo($("#lst-"+ pan[p]));
+								$("#"+ $(this).attr("id")).addClass("btn-block font-weight-bold");
+								$("#lst-"+ pan[p] + ">h2").remove();
+								// $("#lst-"+ pan[p]).parent().find(".tran").show();
+							}
+						}
+						)
+					}
+
+		}
+		);
+</script>
+	<!-- ion.sound finished -->
+	<? } ?>
+	<?php include "footer.php"; ?>
+	</body>
+</html>

aa564f8605071c977d756535c8c42003e2ddc089
cansmile <cansmile@gmail.com>
2020-03-05 22:03:43 +0900

    R10 C2 반영
    교수자 회의대로 반영

---
+++ r10-C2.php
@@ -0,0 +1,265 @@
+<?php include "header.php"; ?>
+	<body>
+<?php include "nav.php"; ?>
+<?php if(ul()) { ?>
+<!-- 보기시작 -->
+<section class="bg-white rounded p-2" style="position: fixed; bottom: 0; z-index: 9999; width: 100%;" id="wahl">
+	<div class="container">
+		<div class="row">
+			<div class="col display-4 bg-<?php echo($color); ?> rounded text-center text-white font-weight-bold col-12">Wahl<small>선택지</small></div>
+			<div class="col-12" id="itms">
+				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm px-3" id="1">
+				der Zwinger
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm px-3" id="2">
+				die Frauenkirche
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm px-3" id="3">
+				Deutsches Hygienemuseum
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm px-3" id="4">
+				die Semperoper
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans6 btn-lg btn-outline-dark itm px-3" id="5">
+				Volkswagen Gläserne Manufaktur
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans8 btn-lg btn-outline-dark itm px-3" id="6">
+				Sächsische Schweiz
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm px-3" id="7">
+				der Zwinger
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm px-3" id="8">
+				die Frauenkirche
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm px-3" id="9">
+				Deutsches Hygienemuseum
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm px-3" id="10">
+				die Semperoper
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans6 btn-lg btn-outline-dark itm px-3" id="11">
+				Volkswagen Gläserne Manufaktur
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans7 btn-lg btn-outline-dark itm px-3" id="12">
+				Sächsische Schweiz
+				</button>
+			</div>
+		</div>
+	</div>
+</section>
+
+		<section>
+			<div class="container">
+							<!-- 고르는 아이템들 -->
+				<div class="row">
+					<div class="col-lg-12 mb-4 mt-2 text-center">
+						<h2>[ <small>문장을 선택하고 알맞은 위치의 노란 단추를 누르세요.</small> ]</h2>
+					</div>
+				</div>
+				<div class="row">
+			</div>
+				<div class="row mt-2">
+					<div class="col-12 border border-dark bg-secondary text-white font-weight-bold rounded p-1 pt-3 pl-3 mt-5">
+						<span class="border border-white p-1 px-2 rounded bg-dark">1</span>
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-1">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>?
+					</div>
+					<div class="col-12 p-3" style="line-height: 250%;">
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-2">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>
+						gehört zu den beeindruckendsten Bauwerken des Barocks. Im Februar 1945 wurde der Zwinger durch die Bombenangriffe auf Dresden fast ganz zerstört. Nach dem Wiederaufbau konnte man ab 1960 bereits einige Teile wieder eröffnen. Heute findet man dort eine Porzellansammlung, den Mathematisch-Physikalischen Salon und die Gemäldegalerie Alte Meister.
+						<span class="tran"><br><small>츠빙어는 바로크 시대의 가장 인상적인 건축물 중 하나입니다. 1945년 2월, 츠빙어는 드레스덴의 폭격으로 거의 완전히 파괴되었습니다. 재건 후 일부는 1960년부터 개방될 수 있었습니다. 오늘날에는 그곳에 도자기박물관, 수학-물리학박물관 및 알테 마이스터 회화관이 있습니다.</small></span>
+					</div>
+					<div class="col-12 border border-dark bg-secondary text-white font-weight-bold rounded p-1 pt-3 pl-3 mt-5">
+						<span class="border border-white p-1 px-2 rounded bg-dark">2</span>
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-3">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>?
+					</div>
+					<div class="col-12 p-3" style="line-height: 250%;">
+						Die
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-3">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>
+						in Dresden ist eine evangelischlutherische Kirche des Barocks. Sie wurde von 1726 bis zum Jahre 1743 erbaut und ist ein Symbol des Dresdner Barocks. Sie wurde am Ende des zweiten Weltkriegs schwer beschädigt. 1994, nach der Wende, begann der Wiederaufbau. Heute ist sie demnach ein offizielles Symbol der Versöhnung.
+						<span class="tran"><br><small>드레스덴의 프라우엔교회는 바로크시대의 루터교 교회입니다. 1726 년에서 1743 년까지 지어졌으며 드레스덴 바로크의 상징입니다. 제 2 차 세계 대전이 끝날 무렵 훼손되었습니다. 통독 이후 1994년에 재건이 시작되었습니다. 그런 이유에서 오늘날엔 화해의 공식적인 상징입니다</small></span>
+					</div>
+					<div class="col-12 border border-dark bg-secondary text-white font-weight-bold rounded p-1 pt-3 pl-3 mt-5">
+						<span class="border border-white p-1 px-2 rounded bg-dark">3</span>
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-4">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>?
+					</div>
+					<div class="col-12 p-3" style="line-height: 250%;">
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-4">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>
+						wurde 1912 gegründet und ist ein Unikat unter Europas Museen. Hier gibt es reichliches Entertainment für jedermann aus dem Bereich der Kultur, Wissenschaft oder Gesellschaft. Angeboten wird zudem auch die populärwissenschaft liche Ausstellung „Abenteuer Mensch“ mit der Gläsernen Frau oder das interaktive Kinder- Musuem „Welt der Sinne“.
+						<span class="tran"><br><small>독일 위생박물관은 1912 년에 설립되었으며 유럽 박물관 중에서 독특합니다. 문화, 과학 또는 사회 분야의 모든 사람들을 위한 풍부한 엔터테인먼트가 있습니다. 글레저네 프라우과 함께 대중 과학 전시회인 "아벤토이어 멘쉬" 또는 대화식 어린이박물관 "벨트 데어 진네"도 제공됩니다.</small></span>
+					</div>
+					<div class="col-12 border border-dark bg-secondary text-white font-weight-bold rounded p-1 pt-3 pl-3 mt-5">
+						<span class="border border-white p-1 px-2 rounded bg-dark">4</span>
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-5">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>?
+					</div>
+					<div class="col-12 p-3" style="line-height: 250%;">
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-5">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>
+						in Dresden ist das Opernhaus der Sächsischen Staatsoper Dresden, mit einer langen geschichtlichen Tradition. Die Semperoper findet man am Theaterplatz im historischen Stadtkern von Dresden in der Nähe der Elbe. Sie ist nach ihrem Architekten Gottfried Semper benannt worden.
+						<span class="tran"><br><small>드레스덴의 젬퍼오페라하우스 (Semperoper)는 오랜 전통을 가진 색슨 주립 오페라 드레스덴의 오페라 하우스입니다. 젬퍼오페라하우스는 엘베강(Elbe) 근처의 드레스덴(Dresden)의 유서 깊은 도심에 있는 테아터플라츠(Theaterplatz)에 있습니다. 그것은 건축가 고트프리트 젬퍼의 이름을 따서 명명되었습니다.</small></span>
+					</div>
+					<div class="col-12 border border-dark bg-secondary text-white font-weight-bold rounded p-1 pt-3 pl-3 mt-5">
+						<span class="border border-white p-1 px-2 rounded bg-dark">5</span>
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-6">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>?
+					</div>
+					<div class="col-12 p-3" style="line-height: 250%;">
+						Die
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-6">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>
+						in Dresden ist eines von drei Produktionsstätten der Volkswagen Sachsen GmbH. Hier wurde von 2002 bis 2016 der Phaeton hergestellt, seit April 2017 der Elektro-Golf der Marke Volkswagen. Zugleich entwickelt sich die Manufaktur zum Center of Future Mobility. Mitte 2018 waren am Standort 380 Personen beschäftigt.
+						<span class="tran"><br><small>드레스덴의 폴크스바겐 투명 공장은 폴크스바겐 작센 유한회사의 세 가지 생산 시설 중 하나입니다. 페톤은 2002년부터 2016년까지 이곳에서 생산되었는데, 2017년 4월부터 폴크스바겐 브랜드인 엘렉트로-골프입니다. 동시에 이 공장은 미래 이동성의 중심으로 발전하고 있습니다. 2018년 중반에 이 곳에 380명이 고용되었습니다.</small></span>
+					</div>
+					<div class="col-12 border border-dark bg-secondary text-white font-weight-bold rounded p-1 pt-3 pl-3 mt-5">
+						<span class="border border-white p-1 px-2 rounded bg-dark">6</span>
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-7">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>?
+					</div>
+					<div class="col-12 p-3" style="line-height: 250%;">
+						Als
+						<div class="itm-lst 1itm d-inline-block px-0 m-0" style="min-width: 200px;" id="lst-8">
+							<h2 class="btn btn-warning btn-xl ttl d-block">
+							▼ </h2>
+						</div>
+						wird der deutsche Teil des Elbsandsteingebirges in Sachsen bezeichnetsüdöstlich von Dresden beiderseits der Elbe im Landkreis Sächsische Schweiz-Osterzgebirge. Die Landschaft ist durch ungewöhnlichen Felsformen geprägt. Zudem gibt es auch noch viele wunderschöne Burganlagen des 15. Jahrhunderts.
+						<span class="tran"><br><small>작센 스위스는 작센의 엘베사암산맥의 독일 부분입니다- 작센 스위스-동부 에르츠산맥 지역의 엘베강 양쪽에 있는 드레스덴 남동쪽. 풍경은 특이한 바위 모양이 특징입니다. 또한 매우 아름다운 15세기 성도 많이 있습니다.</small></span>
+					</div>
+				</div>
+					<!-- 정답화인 버튼 시작 -->
+					<div class="row">
+						<div class="btn my-3 btn-light col-sm-12 col-md-12 col-lg-12" id="chk">
+							정답확인
+						</div>
+					</div>
+					<!-- 정답확인 버튼 끝 -->
+				</div>
+		</section>
+
+<div id="marg"></div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="./js/jquery-3.4.1.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="./js/popper.min.js"></script>
+<script src="./js/bootstrap.js"></script>
+<script src="./js/taptogroup.js"></script>
+<!-- interact.min.js -->
+<script src="./js/ion.sound.min.js"></script>
+<script>
+	$(".tran").hide();
+	$("#chk").hide();
+
+	$(document).ready(function() {
+					// 정답확인
+					$("#chk").on("click", function() {
+						var na = "";
+						if($("#itms").find("button").length < 1) {
+							$(".tran").show();
+
+							// 정답 확인 div 상자 배경색 속성 없애10
+							$(this).removeClass("btn-light ");
+
+							$(".itm-lst").each(function() {
+								if($(this).find("button.btn")) {
+									$(this).find("button.btn").addClass("text-success");
+								}
+							});
+
+							var qa = $(".itm-lst").length; // 전체 문항 수
+							var qr = $(".text-success").length; // 맞춘 항목 수
+							var pe = (qr / qa) * 100; // 정답 비율
+							var tcl = "white"; // 기본 문자색
+
+							// 분류 기준은 100%, 80%, 60%, 40%
+							if(pe > 99) {
+								var st = "원어민이세요?";
+								var cl = "lime";
+								var tcl = "dark";
+							} else if(pe > 74) {
+								var st = "어! 좀 하시는데요~^^";
+								var cl = "success";
+							} else if(pe > 49) {
+								var st = "쓰읍~ 다시 해 보실까요!";
+								var cl = "primary";
+							} else {
+								var st = "좀 더 분발해 주세요";
+								var cl = "danger";
+							}
+
+							$(this).addClass("btn-" + cl + " text-" + tcl);
+							$(this).html("<h4>" + qa + "문제 중 " + qr + "개를 맞히셨네요!<br>" + st + "</h4>");
+
+							$(".btn-lg").text().appendTo($(this).closest("td"));
+							$(".btn-lg").remove();
+						} else {
+							$("div.itm-lst").each(function(idx) {
+								if(!$(this).find("button").length) {
+									if(na != "") {
+										na += ", ";
+									}
+									na += (idx+1);
+								}
+							});
+							alert("모든 문제를 풀어주세요!");
+							// alert(na+"번 문제를 풀어주세요!");
+						}
+					});
+
+				<?php include "wahl.php"; ?>
+
+					var pan = new Array();
+					// pan = ["1","2","3","4","5","6","7","8","9","10"];
+					pan = [1,2,7];
+					var il = $("#itms>.itm").length;
+					for(var p = 0; p < pan.length; p++) {
+					var pani = "#lst-" + pan[p];
+						$(".itm").each(function() {
+							if($(this).hasClass("ans" + pan[p])) {
+								$("#" + $(this).attr("id")).insertBefore($("#lst-" + pan[p] + ">h2"));
+								$("#" + $(this).attr("id")).addClass("btn-inline-block btn-light");
+								$("#" + $(this).attr("id")).css("min-width","100%");
+								$("#" + $(this).attr("id")).css("position","relative");
+								$("#" + $(this).attr("id")).css("left","-6px");
+								$("#" + $(this).attr("id")).css("top","-6px");
+								$("#lst-" + pan[p] + ">h2").remove();
+								// $("#lst-" + pan[p]).closest("tr").find(".tran").show();
+							}
+						})
+					}
+	});
+
+		</script>
+		<!-- ion.sound finished -->
+<? } ?>
+<?php include "footer.php"; ?>
+	</body>
+</html>

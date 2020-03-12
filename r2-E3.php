84d714c6a05d496dda52245a0097f92c2a1a8fef
cansmile <cansmile@gmail.com>
2020-03-13 01:33:47 +0900

    R2 E3 수정 반영
    보기를 하나로 통일하여 접미만 쓰도록 변경하고 1, 4, 7 미리 넣어놓기

--- r2-E3.php
+++ r2-E3.php
@@ -9,21 +9,22 @@
 						<h2>[ <small>정답을 입력하면 입력란이 초록색으로 표시되고,<br> 오답이 될 때는 입력란이 붉게 표시됩니다.</small> ]</h2>
 					</div>
 				</div>
+				<div class="row border border-dark rounded py-2">
+					<div class="col text-center">e</div>
+					<div class="col text-center">en</div>
+					<div class="col text-center">st</div>
+					<div class="col text-center">t</div>
+				</div>
 				<div class="row">
 					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-						<table class="table">
-							<thead>
-								<tr>
-									<th colspan="2" scope="col"><span class="py-2 px-2 d-inline-block">komme</span><span class="py-2 px-2 d-inline-block">kommt</span><span class="py-2 px-2 d-inline-block">kommen</span></th>
-								</tr>
-							</thead>
+						<table class="table m-0">
 							<tbody>
 								<tr>
 									<th class="border-top-0 border-bottom-0">1.</th>
 									<td class="border-top-0 border-bottom-0">
 										<span class="sen"><div class="input-group">
 												Frau Kim
-												<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 mx-1" style="position: relative; top: -6px;" aria-label="." id="qst-1">
+												komm<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 ml-0 mr-1 col-2 pl-0" style="position: relative; top: -6px;" aria-label="." id="qst-1">
 												aus Korea.
 											</div></span>
 										<span class="tran"><small>김 선생님은 한국 출신입니다.</small></span>
@@ -34,7 +35,7 @@
 									<td class="border-top-0 border-bottom-0">
 										<span class="sen"><div class="input-group">
 												Woher
-												<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 mx-1" style="position: relative; top: -6px;" aria-label="." id="qst-2">
+												komm<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 ml-0 mr-1 col-2 pl-0" style="position: relative; top: -6px;" aria-label="." id="qst-2">
 												Sie?
 											</div></span>
 										<span class="tran"><small>당신은 어디에서 왔나요?</small></span>
@@ -45,7 +46,7 @@
 									<td class="border-top-0 border-bottom-0">
 										<span class="sen"><div class="input-group">
 												Ich
-												<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 mx-1" style="position: relative; top: -6px;" aria-label="." id="qst-3">
+												komm<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 ml-0 mr-1 col-2 pl-0" style="position: relative; top: -6px;" aria-label="." id="qst-3">
 												aus Japan.
 											</div></span>
 										<span class="tran"><small>저는 일본 출신입니다.</small></span>
@@ -55,19 +56,14 @@
 						</table>
 					</div>
 					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-						<table class="table">
-							<thead>
-								<tr>
-									<th colspan="2" scope="col"><span class="py-2 px-2 d-inline-block">heiße</span><span class="py-2 px-2 d-inline-block">heißt</span><span class="py-2 px-2 d-inline-block">heißen</span></th>
-								</tr>
-							</thead>
+						<table class="table m-0">
 							<tbody>
 								<tr>
 									<th class="border-top-0 border-bottom-0">4.</th>
 									<td class="border-top-0 border-bottom-0">
 										<span class="sen"><div class="input-group">
 												Ich
-												<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 mx-1" style="position: relative; top: -6px;" aria-label="." id="qst-4">
+												heiß<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 ml-0 mr-1 col-2 pl-0" style="position: relative; top: -6px;" aria-label="." id="qst-4">
 												Chul.
 											</div></span>
 										<span class="tran"><small>저는 철이에요.</small></span>
@@ -78,7 +74,7 @@
 									<td class="border-top-0 border-bottom-0">
 										<span class="sen"><div class="input-group">
 												Wie
-												<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 mx-1" style="position: relative; top: -6px;" aria-label="." id="qst-5">
+												heiß<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 ml-0 mr-1 col-2 pl-0" style="position: relative; top: -6px;" aria-label="." id="qst-5">
 												du?
 											</div></span>
 										<span class="tran"><small>네 이름은 뭐야?</small></span>
@@ -89,7 +85,7 @@
 									<td class="border-top-0 border-bottom-0">
 										<span class="sen"><div class="input-group">
 												Wie
-												<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 mx-1" style="position: relative; top: -6px;" aria-label="." id="qst-6">
+												heiß<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 ml-0 mr-1 col-2 pl-0" style="position: relative; top: -6px;" aria-label="." id="qst-6">
 												Sie?
 											</div></span>
 										<span class="tran"><small>당신의 이름은 무엇인가요?</small></span>
@@ -99,19 +95,14 @@
 						</table>
 					</div>
 					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-						<table class="table">
-							<thead>
-								<tr>
-									<th colspan="2" scope="col"><span class="py-2 px-2 d-inline-block">wohne</span><span class="py-2 px-2 d-inline-block">wohnt</span><span class="py-2 px-2 d-inline-block">wohnst</span></th>
-								</tr>
-							</thead>
+						<table class="table m-0">
 							<tbody>
 								<tr>
 									<th class="border-top-0 border-bottom-0">7.</th>
 									<td class="border-top-0 border-bottom-0">
 										<span class="sen"><div class="input-group">
 												Wo
-												<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 mx-1" style="position: relative; top: -6px;" aria-label="." id="qst-7">
+												wohn<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 ml-0 mr-1 col-2 pl-0" style="position: relative; top: -6px;" aria-label="." id="qst-7">
 												du?
 											</div></span>
 										<span class="tran"><small>너는 어디 살아?</small></span>
@@ -122,7 +113,7 @@
 									<td class="border-top-0 border-bottom-0">
 										<span class="sen"><div class="input-group">
 												Ich
-												<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 mx-1" style="position: relative; top: -6px;" aria-label="." id="qst-8">
+												wohn<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 ml-0 mr-1 col-2 pl-0" style="position: relative; top: -6px;" aria-label="." id="qst-8">
 												in Salzburg.
 											</div></span>
 										<span class="tran"><small>나는 잘츠부르그에 살아.</small></span>
@@ -133,7 +124,7 @@
 									<td class="border-top-0 border-bottom-0">
 										<span class="sen"><div class="input-group">
 												Stefan
-												<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 mx-1" style="position: relative; top: -6px;" aria-label="." id="qst-9">
+												wohn<input type="text" class="form-control q border-left-0 border-top-0 border-right-0 rounded-0 ml-0 mr-1 col-2 pl-0" style="position: relative; top: -6px;" aria-label="." id="qst-9">
 												in Graz.
 											</div></span>
 										<span class="tran"><small>스테판은 그라츠에 살아.</small></span>
@@ -153,9 +144,9 @@
 
 		</div>
 	</section>
-		
 
-		
+
+
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="./js/jquery-3.4.1.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -167,7 +158,7 @@
 			$(".tran").hide();
 
 			var an = new Array();
-			var an = ["kommt","kommen","komme","heiße","heißt","heißen","wohnst","wohne","wohnt"];
+			var an = ["t","en","e","e","t","en","st","e","t"];
 
 			$(document).ready(function() {
 /* 입력하는 문자 확인(정답 표시 없음) 여기부터 */
@@ -179,7 +170,7 @@ function rfchk(th,io) {
 	a = th.val();
 	a = a.replace(/ /gi, "");
 	if(!$.isArray(an[qn])) {
-		// 1 인 경우 
+		// 1 인 경우
 		if(io) {
 			b = an[qn];
 		} else {
@@ -200,12 +191,12 @@ function rfchk(th,io) {
 				b = an[qn][fd].substr(0,q);
 			}
 			b = b.replace(/ /gi, "");
-			
+
 			if(a == b) {
 				return true;
 			}
 		}
-		
+
 	}
 }
 				$(".q").on("keyup", function () {
@@ -229,7 +220,7 @@ function rfchk(th,io) {
 						$(this).removeClass("bg-success");
 						$(this).removeClass("text-white font-weight-bold");
 					}
-					
+
 					if($(this).val()) {
 						$("#ant-"+$(this).attr("id").substr(4)).show();
 						$("#ant-"+$(this).attr("id").substr(4)).text($(this).val());
@@ -377,14 +368,20 @@ function rfchk(th,io) {
 						;
 					})
 
-				$("#qst-1").val(an[0]);
-				$("#qst-4").val(an[3]);
-				$("#qst-1").addClass("bg-success text-white font-weight-bold");
-				$("#qst-4").addClass("bg-success text-white font-weight-bold");
-				$("#qst-1").prop("disabled",true);
-				$("#qst-4").prop("disabled",true);
+					var pan=new Array();
+					// pan=[1,2,3,4,5,6,7,8,9,10];
+					pan = [1,4,7];
+					for(var p=0;
+					p < pan.length;
+					p++) {
+						var pann="#qst-"+pan[p];
+						$(pann).val(an[(pan[p]-1)]);
+						$(pann).addClass("bg-success text-white font-weight-bold");
+						$(pann).prop("disabled", true);
+						// $(pann).closest("tr").find(".tran").show();
+					}
 			});
-			
+
 		</script>
 		<!-- ion.sound finished -->
 <? } ?>

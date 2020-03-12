83d8f6710b971cf0abe777b46a3175b713cd03fb
cansmile <cansmile@gmail.com>
2020-03-13 02:35:01 +0900

    R2 E5 수정 반영
    번역 일부 수정하고 미리 넣기 굵게하기 제거

--- r2-E5.php
+++ r2-E5.php
@@ -12,7 +12,7 @@
 					a. In Daejeon.<span class="tran"><br>&nbsp;<small>대전에서.</small></span>
 					</button>
 					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark so itm" id="2">
-					b. Nein, in Suwon.<span class="tran"><br>&nbsp;<small>아니 난 수원에.</small></span>
+					b. Nein, in Suwon.<span class="tran"><br>&nbsp;<small>아니, 수원에서.</small></span>
 					</button>
 					<button type="button" class="mt-1 mx-1 btn ans6 btn-lg btn-outline-dark so itm" id="3">
 					c. Ja, ich spreche gut Englisch.<span class="tran"><br>&nbsp;<small>응, 나 영어 잘해.</small></span>
@@ -96,7 +96,7 @@
 							</tr>
 							<tr>
 								<th class="bg-light" scope="row">6</th>
-								<td>Sprichst du gut Englisch?<span class="tran"><br>&nbsp;<small>너 영어 잘 말하니?</small></span></td>
+								<td>Sprichst du gut Englisch?<span class="tran"><br>&nbsp;<small>너 영어 잘하니?</small></span></td>
 								<td>
 									<div class="itm-lst 1itm" id="lst-6">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
@@ -125,22 +125,6 @@
 				</div>
 			</div>
 			<!-- 정답확인 버튼 끝 -->
-			<div class="row">
-				<div class="col">
-					<table class="table text-center">
-						<thead>
-							<th scope="col">
-								<span class="bg-lime font-weight-bold rounded p-1 px-2 m-1 text-left">TIPP</span>※ 참고
-							</th>
-						</thead>
-						<tbody>
-							<tr>
-								<td><a href="./r2-Grammatik und Wendungen.php">Grammatik und Wendungen</a></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
 		</div>
 	</section>
 	<div id="marg"></div>
@@ -261,7 +245,7 @@
 	$(".itm").each(function() {
 	if($(this).hasClass("ans"+ pan[p])) {
 	$("#"+ $(this).attr("id")).appendTo($("#lst-"+ pan[p]));
-	$("#"+ $(this).attr("id")).addClass("btn-block font-weight-bold text-left");
+	$("#"+ $(this).attr("id")).addClass("btn-block text-left");
 	// $("#"+ $(this).attr("id")).addClass("border-0");
 	$("#lst-"+ pan[p] + ">h2").remove();
 	// $("#lst-"+ pan[p]).closest("tr").find(".tran").show();

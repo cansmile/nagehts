711ebde3eef8c0b833912a3777e90680a914af7a
cansmile <cansmile@gmail.com>
2020-03-13 17:20:11 +0900

    R10 B3a 수정 반영
    음원 추가, 단어별 시간 입력

--- r10-B3a.php
+++ r10-B3a.php
@@ -3,41 +3,44 @@
 <body>
 	<?php include "nav.php"; ?>
 	<?php if(ul()) { ?>
+	<!-- 알림 시작 -->
+	<?php require_once "ready.php"; ?>
+	<!-- 알림 끝 -->
 	<!-- 보기시작 -->
 	<section class="bg-white rounded p-2" style="position: fixed; bottom: 0; z-index: 9999; width: 100%;" id="wahl">
 		<div class="container">
 			<div class="row">
 				<div class="col display-4 bg-<?php echo($color); ?> rounded text-center text-white font-weight-bold col-12">Wahl</div>
 				<div class="col-12" id="itms">
-					<button type="button" class="mt-1 mx-1 btn ans8 btn-lg btn-outline-dark itm" id="1">
+					<button type="button" class="mt-1 mx-1 btn ans8 btn-lg btn-outline-dark itm so" id="1">
 						der Zug
 						<span class="tran"><br><small>기차</small></span>
 					</button>
-					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="2">
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm so" id="2">
 						das Taxi
 						<span class="tran"><br><small>택시</small></span>
 					</button>
-					<button type="button" class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm" id="3">
+					<button type="button" class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm so" id="3">
 						das Auto
 						<span class="tran"><br><small>자동차</small></span>
 					</button>
-					<button type="button" class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm" id="4">
+					<button type="button" class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm so" id="4">
 						die Straßenbahn
 						<span class="tran"><br><small>전철</small></span>
 					</button>
-					<button type="button" class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm" id="5">
+					<button type="button" class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm so" id="5">
 						das Fahrrad
 						<span class="tran"><br><small>자전거</small></span>
 					</button>
-					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="6">
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm so" id="6">
 						das Flugzeug
 						<span class="tran"><br><small>비행기</small></span>
 					</button>
-					<button type="button" class="mt-1 mx-1 btn ans7 btn-lg btn-outline-dark itm" id="7">
+					<button type="button" class="mt-1 mx-1 btn ans7 btn-lg btn-outline-dark itm so" id="7">
 						die U-Bahn
 						<span class="tran"><br><small>지하철</small></span>
 					</button>
-					<button type="button" class="mt-1 mx-1 btn ans6 btn-lg btn-outline-dark itm" id="8">
+					<button type="button" class="mt-1 mx-1 btn ans6 btn-lg btn-outline-dark itm so" id="8">
 						das Schiff
 						<span class="tran"><br><small>배</small></span>
 					</button>
@@ -50,7 +53,12 @@
 			<!-- 고르는 아이템들 -->
 			<div class="row">
 				<div class="col-lg-12 mb-4 mt-2 text-center">
-					<h3>[ <small>단어를 선택하고 알맞은 위치의 노란 단추를 누르세요.</small> ]</h3>
+					<h3>[ <small>단어를 선택하고 알맞은 위치의 노란 단추를 누르세요.</small> ]
+					<button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so" id="0">
+					HV
+					</button><button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so" id="0_p">
+					❚❚
+					</button></h3>
 				</div>
 			</div>
 			<div class="row">
@@ -207,7 +215,112 @@
 		$("#0_p").hide();
 		$(".tran").hide();
 		$("#chk").hide();
+
+		// 각 문장 재생 횟수 초기화
+		var hm=new Array(), sen=new Array();
+		$(".so").each(function() {
+			hm[$(this).attr("id")]=0;
+			sen[$(this).attr("id")]=$("#"+$(this).attr("id")).html();
+		});
+
 		$(document).ready(function() {
+
+				ion.sound( {
+					sounds : [ {
+						name : "r10 B3a",
+						sprite : {
+							"0": [.724, 32.137],
+							"1": [10.818, .962],
+							"2": [7.772, 1.087],
+							"3": [13.902, 1.169],
+							"4": [17.274, 1.504],
+							"5": [21.276, 1.163],
+							"6": [24.429, 1.419],
+							"7": [27.927, 1.355],
+							"8": [31.348, 1.190]
+						}
+					}
+					, {
+						name: "dingdongdang",
+							path: "sounds/"
+					}
+					, {
+						name: "Cartoon_Boing",
+							path: "sounds/"
+					}
+					],
+					path : "sounds/Reihe 10/",
+					preload : true,
+					volume : 1.0,
+					multiplay: false,
+					ended_callback: function(obj) {
+						// 재생이 끝날 때 2번 이상이면 번역 보이기
+						hmn=obj.part;
+						hm[hmn]++;
+						// 전체 재생 끝나면 일시정지 버튼 숨기고 HV 버튼 보이기
+						if(obj.part=="0") {
+							$("#0").show();
+							$("#0_p").hide();
+							if(hm[hmn] > 1) {
+								$(".tran").show();
+							}
+						}
+						else {
+							$("#"+obj.part).html(sen[obj.part]);
+							if(hm[hmn] > 1) {
+								$("#"+hmn).closest(".btn").find(".tran").show();
+							}
+						}
+					}
+					, ready_callback: function () {
+
+						$(".so").on("click", function () {
+							if($(this).attr("id").substr(-2)=="_p") {
+								// _p 붙어 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기
+								ion.sound.pause("r10 B3a", {
+									part: "0"
+								}
+								);
+								$("#0").show();
+								$(this).hide();
+							}
+							else if($(this).text()==sen[$(this).attr("id")]) {
+								// 재생되고 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기
+								ion.sound.play("r10 B3a", {
+									part: $(this).attr("id")
+								}
+								);
+								$(this).html("❚❚");
+							}
+							else if($(this).html()=="❚❚") {
+								// 재생되고 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기
+								ion.sound.pause("r10 B3a", {
+									part: $(this).attr("id")
+								}
+								);
+								$(this).html("▶");
+							}
+							else {
+								// _p 붙어 있지 않으면 id 그대로 재생
+								ion.sound.play("r10 B3a", {
+									part: $(this).attr("id")
+								}
+								);
+								// 전체 듣기 재생일 때는 일시정지 버튼 보이기
+								if($(this).attr("id")=="0") {
+									$(this).hide();
+									$("#0_p").show();
+								}
+								;
+							}
+							;
+						}
+						)
+						;
+
+
+
+
 					// 정답확인
 					$("#chk").on("click", function() {
 						var na="";
@@ -281,7 +394,7 @@
 						$(".itm").each(function() {
 							if($(this).hasClass("ans"+ pan[p])) {
 								$("#"+ $(this).attr("id")).appendTo($("#lst-"+ pan[p]));
-								$("#"+ $(this).attr("id")).addClass("btn-block font-weight-bold");
+								$("#"+ $(this).attr("id")).addClass("btn-block");
 								$("#lst-"+ pan[p] + ">h2").remove();
 								// $("#lst-"+ pan[p]).parent().find(".tran").show();
 							}
@@ -289,6 +402,17 @@
 						)
 					}
 
+		
+					
+
+
+
+						$("#0").show();
+						$(".alert").hide();
+					}
+				}
+				);
+			
 		}
 		);
 </script>

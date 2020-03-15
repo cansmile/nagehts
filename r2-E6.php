30178ab74504710b0e98953571d10d4648e97ed8
cansmile <cansmile@gmail.com>
2020-03-16 00:20:43 +0900

    R2 E6 수정
    정답 클래스 변경

--- r2-E6.php
+++ r2-E6.php
@@ -206,18 +206,13 @@
 		var na="";
 		if($("#itms").find("button").length < 1) {
 			$(".tran").show();
-			$(".itm-lst").each(function() {
-			$(this).html($(this).find("button").html());
-				// $(this).addClass("font-weight-bold bg-white border rounded border-dark");
-			}
-			);
-
 
 			// 정답 확인 div 상자 배경색 속성 없애기
 			$(this).removeClass("btn-light ");
+			$(".itm-lst>button").addClass("text-success font-weight-bold");
 
 			var qa = $(".itm-lst").length; // 전체 문항 수
-			var qr = $(".itm-lst>.tran").length; // 맞춘 항목 수
+			var qr = $(".text-success").length; // 맞춘 항목 수
 			var pe = (qr / qa) * 100; // 정답 비율
 			var tcl = "white"; // 기본 문자색
 

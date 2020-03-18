4b5968b2521f4bf85ec126e5389f0acf9994c1bb
cansmile <cansmile@gmail.com>
2020-03-18 10:43:12 +0900

    R1 F1 수정
    howler로 전환하고 항목별 시간 입력

--- r1-F1.php
+++ r1-F1.php
@@ -1,4 +1,4 @@
-<?php include "header.php"; ?>
+2<?php include "header.php"; ?>
 	<body>
 <?php include "nav.php"; ?>
 <?php if(ul()) { ?>
@@ -12,10 +12,8 @@
 						<h2>Hören Sie und sprechen Sie nach.<br>
 							<p>
 								듣고 따라하세요
-							<button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline itm" id="0">
+							<button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so" id="0">
 							HV
-						</button><button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline itm" id="0_p">
-							❚❚
 						</button>
 						</p>
 						</h2>
@@ -23,15 +21,15 @@
 
 				<div class="row">
 					<div class="col-lg-12 mb-4 mt-2 text-center">
-						<h3>[ <small><button type="button" class="btn disabled btn-sm btn-<?php echo($color); ?>">HV</button> 버튼 또는 <button type="button" class="so btn btn-sm btn-outline-secondary disabled">▶</button> 버튼을 눌러 들으면 해당 음성이 재생됩니다. 단어를 누르면 그림이 나옵니다.</small> ]</h3>
+						<h3>[ <small><button type="button" class="btn disabled btn-sm btn-<?php echo($color); ?>">HV</button> 버튼 또는 <button type="button" class="btn btn-sm btn-outline-secondary disabled">단어</button> 버튼을 눌러 들으면 해당 음성이 재생됩니다. 단어를 누르면 그림이 나옵니다.</small> ]</h3>
 					</div>
 				</div>
 
 				<div class="accordion row" id="accordionitms">
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingOne">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingOne">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseOne" area-expanded="false" aria-controls="collapseOne" id="1">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseOne" area-expanded="false" aria-controls="collapseOne" id="1">
 									eins
 								</button>
 							</h5>
@@ -40,10 +38,10 @@
 							<img src=".\images\Reihe 1\Reihe-1-F1-1.png" style="max-width: 100%; height: auto;" class="d-block mx-auto">
 						</div>
 					</div>
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingTwo">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingTwo">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseTwo" area-expanded="false" aria-controls="collapseTwo" id="2">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseTwo" area-expanded="false" aria-controls="collapseTwo" id="2">
 									zwei
 								</button>
 							</h5>
@@ -52,10 +50,10 @@
 							<img src=".\images\Reihe 1\Reihe-1-F1-2.png" style="max-width: 100%; height: auto;" class="d-block mx-auto">
 						</div>
 					</div>
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingThree">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingThree">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseThree" area-expanded="false" aria-controls="collapseThree" id="3">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseThree" area-expanded="false" aria-controls="collapseThree" id="3">
 									drei
 								</button>
 							</h5>
@@ -64,10 +62,10 @@
 							<img src=".\images\Reihe 1\Reihe-1-F1-3.png" style="max-width: 100%; height: auto;" class="d-block mx-auto">
 						</div>
 					</div>
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingFour">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingFour">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseFour" area-expanded="false" aria-controls="collapseFour" id="4">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseFour" area-expanded="false" aria-controls="collapseFour" id="4">
 									vier
 								</button>
 							</h5>
@@ -76,10 +74,10 @@
 							<img src=".\images\Reihe 1\Reihe-1-F1-4.png" style="max-width: 100%; height: auto;" class="d-block mx-auto">
 						</div>
 					</div>
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingFive">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingFive">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseFive" area-expanded="false" aria-controls="collapseFive" id="5">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseFive" area-expanded="false" aria-controls="collapseFive" id="5">
 									fünf
 								</button>
 							</h5>
@@ -88,10 +86,10 @@
 							<img src=".\images\Reihe 1\Reihe-1-F1-5.png" style="max-width: 100%; height: auto;" class="d-block mx-auto">
 						</div>
 					</div>
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingSix">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingSix">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseSix" area-expanded="false" aria-controls="collapseSix" id="6">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseSix" area-expanded="false" aria-controls="collapseSix" id="6">
 									sechs
 								</button>
 							</h5>
@@ -100,10 +98,10 @@
 							<img src=".\images\Reihe 1\Reihe-1-F1-6.png" style="max-width: 100%; height: auto;" class="d-block mx-auto">
 						</div>
 					</div>
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingSeven">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingSeven">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseSeven" area-expanded="false" aria-controls="collapseSeven" id="7">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseSeven" area-expanded="false" aria-controls="collapseSeven" id="7">
 									sieben
 								</button>
 							</h5>
@@ -112,10 +110,10 @@
 							<img src=".\images\Reihe 1\Reihe-1-F1-7.png" style="max-width: 100%; height: auto;" class="d-block mx-auto">
 						</div>
 					</div>
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingEight">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingEight">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseEight" area-expanded="false" aria-controls="collapseEight" id="8">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseEight" area-expanded="false" aria-controls="collapseEight" id="8">
 									acht
 								</button>
 							</h5>
@@ -124,10 +122,10 @@
 							<img src=".\images\Reihe 1\Reihe-1-F1-8.png" style="max-width: 100%; height: auto;" class="d-block mx-auto">
 						</div>
 					</div>
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingNine">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingNine">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseNine" area-expanded="false" aria-controls="collapseNine" id="9">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseNine" area-expanded="false" aria-controls="collapseNine" id="9">
 									neun
 								</button>
 							</h5>
@@ -136,10 +134,10 @@
 							<img src=".\images\Reihe 1\Reihe-1-F1-9.png" style="max-width: 100%; height: auto;" class="d-block mx-auto">
 						</div>
 					</div>
-					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0">
-						<div class="card-header" id="headingTen">
+					<div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl px-0 border-0">
+						<div class="card-header bg-white" id="headingTen">
 							<h5 class="mb-0">
-								<button class="btn btn-outline-dark btn-block btn-md mt-1 itm" type="button" data-toggle="collapse" data-target="#collapseTen" area-expanded="false" aria-controls="collapseTen" id="10">
+								<button class="btn btn-outline-dark btn-block btn-md mt-1 so" type="button" data-toggle="collapse" data-target="#collapseTen" area-expanded="false" aria-controls="collapseTen" id="10">
 									zehn
 								</button>
 							</h5>
@@ -152,91 +150,122 @@
 			</div>
 		</section>
 		
-
+<div id="last" class="d-none"></div>
 		
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="./js/jquery-3.4.1.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="./js/popper.min.js"></script>
 		<script src="./js/bootstrap.js"></script>
-		<!-- ion.sound -->
-		<script src="./js/ion.sound.min.js"></script>
+		<script src="./js/howler.core.js"></script>
+<!-- 맞고 틀리는지 소리 -->
+<?php require_once("./oxsound.php"); ?>
 		<script>
 			$("#0").hide();
-			$("#0_p").hide();
+			$(".tran").hide();
+			$(".nu").hide();
 
 			$(document).ready(function() {
+// 소리 출력 전역 변수와 함수
+var sen = new Array(), pa = new Array(), he = new Array(), last;
+$(".so").each(function() {
+	var t = $(this);
+	var ti = t.attr("id");
+	sen[ti] = 0;
+	pa[ti] = t.html();
+});
 
-				ion.sound({
-					sounds : [{
-						name : "r1 F1",
+function stopAll() {
+	$(".so").each(function() {
+		$(this).html(pa[$(this).attr("id")]);
+	});
+}
 
-						sprite : {
-							"0": [7.1,27.4],
-							"1": [11.4,1.2],
-							"2": [14.1,1],
-							"3": [16.8,1],
-							"4": [19.1,1.2],
-							"5": [21.6,1.2],
-							"6": [24.1,1.3],
-							"7": [26.7,1.2],
-							"8": [28.9,1],
-							"9": [31.3,1],
-							"10": [33.3,1.3]
-						}
-					}],
-					path : "sounds/Reihe 1/",
-					preload : true,
-					volume : 1.0,
-					multiplay: false,
-					
-					ended_callback: function(obj) {
-						if(obj.part=="0") {
-							$("#0").show();
-							$("#0_p").hide();
-						};
-					}, ready_callback: function () {
-							$(".itm").on("click", function () {
-								if($(this).attr("id").substr(-2) == "_p") {
-									ion.sound.pause("r1 F1", {
-										part: "0"
-									});
-									$("#0").show();
-									$(this).hide();
-								} else {
-									if($(this).attr("id") != 0) {
-									var ch = "#collapse"+$(this).closest(".card-header").attr("id").substr(7);
-										if(!$(ch).hasClass("show")) {
-											// _p 붙어 있지 않으면 id 그대로 재생
-											ion.sound.play("r1 F1", {
-												part: $(this).attr("id")
-											});
-										}
-									} else {
-											//_p 붙어 있지 않으면 id 그대로 재생
-											ion.sound.play("r1 F1", {
-												part: $(this).attr("id")
-											});
-									}
-									
-									// 전체 듣기 재생일 때는 일시정지 버튼 보이기
-									if($(this).attr("id") == "0") {
-										$(this).hide();
-										$("#0_p").show();
-									};
-								}
-							});
-						// 준비되면 HV 보이기
-						$("#0").show();
-						$(".alert").hide();
-						}
-					})
+// 문제 재생
+var nagehts = new Howl({
+	src: ["./sounds/Reihe 1/r1 F1.mp3"],
+	sprite : {
+		"0": [242, 34112],
+		"1": [11116, 1428],
+		"2": [13755, 1421],
+		"3": [16522, 1256],
+		"4": [18856, 1327],
+		"5": [21257, 1430],
+		"6": [23796, 1484],
+		"7": [26330, 1477],
+		"8": [28673, 1265],
+		"9": [30899, 1240],
+		"10": [32999, 1315]
+	},
+	html5: true,
+	volume: 1,
+	format: "mp3",
+	preload: true,
+	onloaderror: function() {
+		$(".alert").append("<br /><strong class=\"font-weight-bold text-dark display-4\">페이지를 다시 읽어주시기 바래요.</strong>");
+		console.log("다시 읽어주세요!");
+	},
+	onload: function() {
+		// 음성 준비되면 HV 버튼 나타내기 
+		$("#0").show();
+		$(".alert").hide();
+
+		$(".so").on("click", function() {
+				var t = $(this);
+				var ti = t.attr("id");
+
+			if(($("div#last").text() == "" || t.text() == "❚❚") && !t.hasClass(".itm-lst")) {
+				$("#last").text(ti);
+				t.text("■");
+				nagehts.seek();
+				nagehts.play(ti);
+				sen[ti]++;
+	
+				last = ti;
+	
+				$("#cnt-"+ti).text(sen[ti]);
+			} else if(last == ti && nagehts.playing($("div#last").text())) {
+				$("#last").text("");
+				t.html(pa[ti]);
+				nagehts.pause();
+				sen[ti]--;
+				$("#cnt-"+ti).text(sen[ti]);
+			}
+
+		});
+
+
+	$("#0").show();
+	$(".alert").hide();
+
+	},
+	onend: function() {
+		$("div#last").text("");
+		stopAll();
+		$("#cnt-"+last).text(sen[last]);
+		if(last == 0) {
+			if(sen[last] == 2) {
+				$(".tran").show();
+				$(".so").each(function() {
+					pa[last] = $("#"+last).html();
 				});
+			}
+		} else if(sen[last] == 2) {
+			if($(this).hasClass(".itm")) {
+				$("#"+last).find(".tran").show();
+			}
+			$("#"+last).closest("tr").find(".tran").show();
+			pa[last] = $("#"+last).html();
+		}
+	}
 
+
+});
+
+		});
+			
 		</script>
-
-		<!-- ion.sound finished -->
-<? } ?>
+	<? } ?>
 <?php include "footer.php"; ?>
 	</body>
 </html>

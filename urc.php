6858bed6ea88df595909ec6777eff4d4c3c952aa
cansmile <cansmile@gmail.com>
2020-03-20 12:32:15 +0900

    학생 전환 자동화
    한 번에 읽기, 처리하면서 바로 php로 출력

--- urc.php
+++ urc.php
@@ -2,16 +2,31 @@
 $hdr = "nagehts";
 $dbn = "wordpress";
 $fn = "2020_register.txt";
-	$h = fopen("./".$fn, "r");
-	$hh = fgets($h);
-	$em = explode(",", $hh);
+$h = fopen("/var/www/html/nagehts.org/public_html/".$fn, "rb") or die("이상해요");
+$em = explode(",",fgets($h));
+
+$p = fopen("/var/www/html/nagehts.org/public_html/st.php", "w+") or die("이상해요");
+fwrite($p,"
+<!DOCTYPE html>
+<html lang=\"kr\">
+<head>
+	<title>학생들이 어떻게 됐냐?</title>
+	<link href=\"./css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\">
+	<script src=\"./js/jquery-3.4.1.min.js\"></script>
+	<script src=\"./js/popper.min.js\"></script>
+	<script src=\"./js/bootstrap.js\"></script>
+</head>
+<body>
+<section>
+	<div class=\"container\">
+		<div class=\"row\">
+"); // php 출력용
 
 $t = sizeof($em); // 전체 학생
 $done = 0; // 완료된 학생
 $group = "";
 $link = mysqli_connect('localhost', 'root', 'qudwnWkd321!@#');
 if (!$link) { die('연결에 실패했습니다: ' . mysqli_error());}
-// echo 'MySQL 서버에 정상적으로 연결되었습니다.';
 
 
 mysqli_select_db($link, $dbn);
@@ -20,29 +35,43 @@ for($i = 0; $i < sizeof($em); $i++) {
 	$qr = mysqli_query($link, $q);
 	if($qr!=false) {
 		$r = mysqli_fetch_array($qr);
-		// print_r("<br>".$em[$i]." = ".$r[0]);
 		$uid = $r[0];
 		$q = "SELECT `meta_value` FROM `".$hdr."_usermeta` WHERE `user_id` = ".$uid." AND `meta_key` = '".$hdr."_capabilities'";
-		// echo($q."<br>");
 		$qr = mysqli_query($link, $q);
 		if($qr!=false) {
 			$r = mysqli_fetch_array($qr); 
-			// echo("<br>역할: ".$r[0]."<br>");
-			// print_r("<br>".$r."<br>");
 			if($r[0] == "a:1:{s:10:\"subscriber\";b:1;}") {
+				fwrite($p,"<div class=\"col-4 text-center text-success p-2\"><span class=\"border border-dark rounded bg-white text-dark\">".$i."</span>&nbsp;".$em[$i]." = 구독자 </div>");
 				$group .= date("Y-m-d H:i:s", time())."[ ".$em[$i]." ] 처리됨 "."\n";
 				$q = "UPDATE `".$hdr."_usermeta` SET `meta_value` = 'a:1:{s:7:\"student\";b:1;}' WHERE `".$hdr."_usermeta`.`user_id` = ".$uid." and `".$hdr."_usermeta`.`meta_key` = '".$hdr."_capabilities';";
 				$qr = mysqli_query($link, $q);
 				// echo("<br>".$q."<br>");
 				$done++;
-			}
-			if($r[0] == "a:1:{s:7:\"student\";b:1;}") {
+			} else if($r[0] == "a:1:{s:7:\"student\";b:1;}") {
+				fwrite($p,"<div class=\"col-4 text-center text-white bg-success p-2\"><span class=\"border border-dark rounded bg-white text-dark\">".$i."</span>&nbsp;".$em[$i]." = 학생 </div>");
+				$rs = "학생";
 				$done++;
+			} else {
+				fwrite($p,"<div class=\"col-4 text-center text-danger p-2\"><span class=\"border border-dark rounded bg-white text-dark\">".$i."</span>&nbsp;".$em[$i]." = 미등록 </div>");
 			}
+
 		}
 	}
 }
 if($group != "") {
 	echo($group."\n".$t."명 중 ".$done."명 처리됨(".(round(($done/$t)*100,0))."%) - [".date("Y-m-d H:i:s", time())."]\n");
+} else {
+	echo("변화가 없습니다.\n");
 }
+fclose($h);
+// php로 출력
+fwrite($p, "
+		</div>
+	</div>
+</section>
+</body>
+</html>
+");
+fclose($p);
 ?>
+r

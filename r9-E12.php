8c73c3fa892fd24ada0a007699dd29da5a3b317b
cansmile <cansmile@gmail.com>
2020-03-21 20:35:49 +0900

    R9 E12 수정 반영
    보기 추가, 번역 정정

--- r9-E12.php
+++ r9-E12.php
@@ -10,43 +10,51 @@
 			<div class="col-12" id="itms">
 				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="1">
 					am Wochenende haben wir das koreanische Neujahr „Seoulnal“ gefeiert.
-					<span class="tran"><br />우리는 주말에 한국 설날을 지냈어.</span>
+					<span class="tran"><br />우리는 주말에 한국식 “설날”을 지냈어.</span>
 				</button>
 				<button type="button" class="mt-1 mx-1 btn ans6 btn-lg btn-outline-dark itm" id="2">
-					Dabei haben wir einen Umschlag mit Geld bekommen,
-					<span class="tran"><br />식사 후에 나이가 적은 우리들이 어른들께 세배를 하였고 돈이든 봉투를 받았는데, 제일 맘에 들었어.</span>
+					Dabei haben wir einen Umschlag mit Geld bekommen.
+					<span class="tran"><br />그 때 우리는 돈이든 봉투를 받았어.</span>
 				</button>
-				<button type="button" class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm" id="3">
-					Die ganze Familie ist gekommen
+				<button type="button" class="mt-1 mx-1 btn ans10 btn-lg btn-outline-dark itm" id="3">
+					Bis bald.
+					<span class="tran"><br />곧 보자.</span>
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm" id="4">
+					Die ganze Familie ist gekommen.
 					<span class="tran"><br />모든 가족들이 다 왔어.</span>
 				</button>
-				<button type="button" class="mt-1 mx-1 btn ans9 btn-lg btn-outline-dark itm" id="4">
+				<button type="button" class="mt-1 mx-1 btn ans9 btn-lg btn-outline-dark itm" id="5">
 					Lass uns bald mal treffen und ich zeige dir dann ein paar Videos vom Fest.
-					<span class="tran"><br />우리 한번 만나자. 그럼 내가 몇 개의 비디오영상을 보여줄게.</span>
+					<span class="tran"><br />우리 한번 만나자. 그럼 내가 비디오영상을 몇 개 보여줄게.</span>
 				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="5">
+				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="6">
 					Liebe Mina,
-					<span class="tran"><br />사랑하는 민나,</span>
+					<span class="tran"><br />사랑하는 미나,</span>
 				</button>
-				<button type="button" class="mt-1 mx-1 btn ans10 btn-lg btn-outline-dark itm" id="6">
+				<button type="button" class="mt-1 mx-1 btn ans11 btn-lg btn-outline-dark itm" id="7">
 					Meld dich mal bei mir.
-					<span class="tran"><br />연락하렴. 곧 보자.  너의 00</span>
+					<span class="tran"><br />연락하렴.</span>
 				</button>
-				<button type="button" class="mt-1 mx-1 btn ans7 btn-lg btn-outline-dark itm" id="7">
+				<button type="button" class="mt-1 mx-1 btn ans7 btn-lg btn-outline-dark itm" id="8">
 					Danach haben wir verschiedene Spiele gespielt, wie zum Beispiel das traditionelle Spiel ,Yutnori‘.
-					<span class="tran"><br />그리고나서 우리는 여러 가지 놀이를 하였는데 예를 들어 한국전통놀이인 윷놀이를 했어.</span>
+					<span class="tran"><br />그리고나서 우리는 여러 가지 놀이를 하였는데, 예를 들어 한국전통놀이인 윷놀이를 했어.</span>
 				</button>
-				<button type="button" class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm" id="8">
+				<button type="button" class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm" id="9">
 					Nach dem Essen haben wir Jüngeren uns vor den Älteren verbeugt und ihnen ein frohes neues Jahr gewünscht.
-					<span class="tran"><br />우리들은 함께 떡국을 먹었어. 음, 진짜 맛있었어.</span>
+					<span class="tran"><br />식사 후에 나이가 적은 우리들이 어른들께 세배를 하였고 새해 인사를 드렸어.</span>
 				</button>
-				<button type="button" class="mt-1 mx-1 btn ans8 btn-lg btn-outline-dark itm" id="9">
-					Wir haben dabei viel gelacht
-					<span class="tran"><br />우리는 놀이를 하면서 많이 웃었고, 린다 고모와 조카인 유나 그리고 민호삼촌과 함께한 우리 팀이 이겼다. 와우!</span>
+				<button type="button" class="mt-1 mx-1 btn ans8 btn-lg btn-outline-dark itm" id="10">
+					Wir haben dabei viel gelacht.
+					<span class="tran"><br />우리는 놀이를 하면서 많이 웃었어.</span>
 				</button>
-				<button type="button" class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm" id="10">
+				<button type="button" class="mt-1 mx-1 btn ans12 btn-lg btn-outline-dark itm" id="11">
+					Deine ○○○
+					<span class="tran"><br />너의 ○○○</span>
+				</button>
+				<button type="button" class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm" id="12">
 					Wir haben zusammen Reiskuchensuppe gegessen. Hm, das war einfach lecker!
-					<span class="tran"><br />조카들도 고모와 삼촌들도 그리고 우리 할머니 할아버지도 다 왔어.</span>
+					<span class="tran"><br />우리들은 함께 떡국을 먹었어. 음, 진짜 맛있었어.</span>
 				</button>
 			</div>
 		</div>
@@ -58,9 +66,8 @@
 					<!-- 고르는 아이템들 -->
 		<div class="row">
 			<div class="col-lg-12 mb-4 mt-2 text-center">
-				<h2>Hören Sie und vergleichen Sie.<small>듣고 비교하세요.</small></h2>
+				<h2>Hören Sie und vergleichen Sie.<small>&nbsp;듣고 비교하세요.</small></h2>
 				<h3>[ <small>보기를 선택하고 알맞은 위치의 노란 단추를 누르세요.</small> ]</h2>
-				<h3>[ <small><button type="button" class="btn disabled btn-sm btn-<?php echo($color); ?>">HV</button> 버튼 또는 <button type="button" class="so btn btn-sm btn-outline-secondary disabled">단어</button> 버튼을 눌러 듣기를 2번 완료하면 문장의 번역이 나옵니다.</small> ]</h3>
 			</div>
 		</div>
 		<div class="row">
@@ -142,6 +149,22 @@
 						<tr>
 							<td class="border-0">
 							<div class="itm-lst 1itm" id="lst-10">
+											<h2 class="btn btn-warning btn-xl ttl d-block">
+											▼ </h2>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td class="border-0">
+							<div class="itm-lst 1itm" id="lst-11">
+											<h2 class="btn btn-warning btn-xl ttl d-block">
+											▼ </h2>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td class="border-0">
+							<div class="itm-lst 1itm" id="lst-12">
 											<h2 class="btn btn-warning btn-xl ttl d-block">
 											▼ </h2>
 								</div>
@@ -236,15 +259,15 @@
 
 	var pan = new Array();
 	// pan = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14"];
-	pan = [];
+	pan = [1,12];
 	var il = $("#itms>.itm").length;
 	for(var p = 0; p < pan.length; p++) {
 	var pani = "#lst-" + pan[p];
 		$(".itm").each(function() {
 			if($(this).hasClass("ans" + pan[p])) {
 				$("#" + $(this).attr("id")).appendTo($("#lst-" + pan[p]));
-				$("#" + $(this).attr("id")).addClass("btn-block font-weight-bold");
-				$("#" + $(this).attr("id")).addClass("border-0");
+				$("#" + $(this).attr("id")).addClass("btn-block");
+				// $("#" + $(this).attr("id")).addClass("border-0");
 				$("#lst-" + pan[p] + ">h2").remove();
 				// $("#lst-" + pan[p]).parent().find(".tran").show();
 			}

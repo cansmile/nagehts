c00896d35fceee5df4c273aaee931190f583f087
cansmile <cansmile@gmail.com>
2020-03-25 11:38:06 +0900

    R4 B1 수정 반영
    howler로 전환, 선 없는 표 적용

--- r4-B1.php
+++ r4-B1.php
@@ -60,8 +60,6 @@
 					<small> 방에서 </small>
 					<button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so" id="0">
 					HV
-					</button><button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so" id="0_p">
-					❚❚
 					</button>
 					</h2>
 					<h3>[ <small><button type="button" class="btn disabled btn-sm btn-<?php echo($color); ?>">HV</button> 버튼 또는 <button type="button" class="btn disabled btn-sm btn-outline-dark">단어</button> 버튼을 눌러 2번 들으면, 번역이 나옵니다.<br>들으면서 단어를 그림과 연결하세요.</small> ]</h3>
@@ -69,13 +67,13 @@
 			</div>
 			<div class="row">
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-1.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-1.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-1">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -86,13 +84,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-2.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-2.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-2">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -103,13 +101,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-3.png" alt="Was ist das?" style="max-height: 100%; width: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-3.png" alt="Was ist das?" style="max-height: 100%; width: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-3">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -120,13 +118,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-4.png" alt="Was ist das?" style="max-height: 100%; width: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-4.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-4">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -137,13 +135,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-5.png" alt="Was ist das?" style="max-height: 100%; width: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-5.png" alt="Was ist das?" style="max-height: 100%; width: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-5">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -154,13 +152,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-6.png" alt="Was ist das?" style="max-height: 100%; width: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-6.png" alt="Was ist das?" style="max-height: 100%; width: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-6">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -171,13 +169,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-7.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-7.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-7">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -188,13 +186,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-8.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-8.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-8">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -205,13 +203,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-9.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-9.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-9">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -222,13 +220,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-10.png" alt="Was ist das?" style="max-height: 100%; width: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-10.png" alt="Was ist das?" style="max-height: 100%; width: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-10">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -239,13 +237,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-11.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-11.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-11">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -256,13 +254,13 @@
 					</table>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-3 col-xl-2">
-					<table class="table">
+					<table class="table table-borderless">
 						<thead>
-							<th height="200" class="text-center border-0 align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-12.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
+							<th height="200" class="text-center align-middle" scope="col"><img src="./images/Reihe 4/Reihe-4-B1-12.png" alt="Was ist das?" style="max-width: 100%; height: auto;"></th>
 						</thead>
 						<tbody>
 							<tr>
-								<th scope="row" class="border-0">
+								<th scope="row">
 									<div class="itm-lst 1itm" id="lst-12">
 										<h2 class="btn btn-warning btn-xl ttl d-block">
 										▼ </h2>
@@ -284,211 +282,174 @@
 	</section>
 	
 	<div id="marg"></div>
-	
+<div id="last" class="d-none"></div>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="./js/jquery-3.4.1.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="./js/popper.min.js"></script>
 	<script src="./js/bootstrap.js"></script>
-	<script src="./js/taptogroup.js"></script>
-	<!-- interact.min.js -->
-	<script src="./js/ion.sound.min.js"></script>
+	<script src="./js/taptogrouph.js"></script>
+	<script src="./js/howler.core.js"></script>
+	<!-- 맞고 틀리는지 소리 -->
+	<?php require_once("./oxsound.php"); ?>
 	<script>
-		$("#0").hide();
-		$("#0_p").hide();
-		$(".tran").hide();
-		$("#chk").hide();
-		$(document).ready(function() {
-			// 각 문장 재생 횟수 초기화
-			var hm = new Array(), sen = new Array();
+	$("#0").hide();
+	$(".tran").hide();
+	$(document).ready(function() {
+		// 소리 출력 전역 변수와 함수
+		var sen=new Array(),
+		pa=new Array(),
+		he=new Array(),
+		last;
+		$(".so").each(function() {
+			var t=$(this);
+			var ti=t.attr("id");
+			sen[ ti]=0;
+			pa[ ti]=t.html();
+		}
+		);
+		function stopAll() {
 			$(".so").each(function() {
-				hm[$(this).attr("id")] = 0;
-				sen[$(this).attr("id")] = 0;
-			});
-			ion.sound({
-				sounds : [{
-					name : "r4 B1",
-					sprite : {
-						"0": [6.2,54],
-						"12": [35.8,1.9],
-						"11": [10.3,1.5],
-						"10": [31.6,1.5],
-						"2": [53.8,1.9],
-						"7": [23.1,1.7],
-						"3": [49.4,1.7],
-						"5": [57.8,2.2],
-						"8": [41,1.2],
-						"9": [45,1.8],
-						"6": [18.7,1.8],
-						"1": [14.5,1.4],
-						"4": [27.3,1.7]
-					}
-				},{
-					name : "dingdongdang",
-					path : "sounds/"
-				}, {
-					name : "Cartoon_Boing",
-					path : "sounds/"
-				}],
-				path : "sounds/Reihe 4/",
-				preload : true,
-				volume : 1.0,
-				multiplay: false,
-				
-				ended_callback: function(obj) {
-					// 재생이 끝날 때 2번 이상이면 번역 보이기
-					hmn = obj.part;
-					hm[hmn]++;
-					// 전체 재생 끝나면 일시정지 버튼 숨기고 HV 버튼 보이기
-					if(obj.part=="0") {
-						$("#0").show();
-						$("#0_p").hide();
-						if(hm[hmn] > 1) {
-							$(".tran").show();
-						}
-					} else {
-						if(obj.part > 12) {
-							$("#"+obj.part).html("▶");
-						}
-						if(hm[hmn] > 1) {
-																	$("#"+hmn).closest("button").find(".tran").show();
-						}
-					}
-				}, ready_callback: function () {
-					
-			$(".o").on("click", function() {
-				ion.sound.play("dingdongdang");
-			});
-			$(".x").on("click", function() {
-				ion.sound.play("Cartoon_Boing");
-			});
-			$("[data-toggle='popover']").popover({
-				delay : {
-					'hide' : 1000
-				},
-				container : "body"
-			});
-		
-			$(".pop").click(function () {
-				// 가장 먼저 지문에 'an' 넣기
-				if (!$(this).siblings().hasClass("an")) {
-					$(this).addClass("an");
-					$(this).addClass("btn-warning");
-					$(this).parent().children().removeClass("btn-light");
-				};
-	
-				// 문제 풀이 정도 업데이트
-				var perc = Math.round(($(".an").length / $(".q").length) * 100);
-				$(".progress>.bar").attr("width", perc + "%;");
-				
-			});
-			
-		// 팝업 내용 사라지기
-		$(".pop").popover().click(function() {
-			setTimeout(function() {
-				$(".pop").popover('hide');
-			}, 500);
-		});
-		$(".so").on("click", function () {
-			if($(this).attr("id").substr(-2) == "_p") {
-				// _p 붙어 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기
-				ion.sound.pause("r4 B1", {
-					part: "0"
-				});
-				$("#0").show();
-				$(this).hide();
-			} else if($(this).html() == "▶") {
-				// 재생되고 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기
-				ion.sound.play("r4 B1", {
-					part: $(this).attr("id")
-				});
-				$(this).html("❚❚");
-			} else if($(this).html() == "❚❚") {
-				// 재생되고 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기
-				ion.sound.pause("r4 B1", {
-					part: $(this).attr("id")
-				});
-				$(this).html("▶");
-			} else {
-				// _p 붙어 있지 않으면 id 그대로 재생
-				ion.sound.play("r4 B1", {
-					part: $(this).attr("id")
-				});
-				// 전체 듣기 재생일 때는 일시정지 버튼 보이기
-				if($(this).attr("id") == "0") {
-					$(this).hide();
-					$("#0_p").show();
-				};
-			};
-		});
-
-		<?php include "wahl.php"; ?>
-		
-		// 정답확인
-		$("#chk").on("click", function() {
-			var na = "";
-			if($("#itms").find("button").length < 1) {
-				$(".tran").show();
-
-				$(".itm-lst").each(function() {
-					if($(this).find(".so")) {
-						$(this).find(".so").addClass("text-success");
-					}
-				});
-
-
-				// 정답 확인 div 상자 배경색 속성 없애기
-				$(this).removeClass("btn-light ");
-
-				var qa = $(".itm-lst").length; // 전체 문항 수
-				var qr = $(".text-success").length; // 맞춘 항목 수
-				var pe = (qr / qa) * 100; // 정답 비율
-				var tcl = "white"; // 기본 문자색
-
-				// 분류 기준은 100%, 80%, 60%, 40%
-				if(pe > 99) {
-					var st = "원어민이세요?";
-					var cl = "lime";
-					var tcl = "dark";
-				} else if(pe > 74) {
-					var st = "어! 좀 하시는데요~^^";
-					var cl = "success";
-				} else if(pe > 49) {
-					var st = "쓰읍~ 다시 해 보실까요?";
-					var cl = "primary";
-				} else {
-					var st = "좀 더 분발해 주세요~";
-					var cl = "danger";
-				}
-
-				$(this).addClass("btn-" + cl + " text-" + tcl);
-				$(this).html("<h4>" + qa + "문제 중 " + qr + "개를 맞히셨네요!<br>" + st + "</h4>");
-
-				$(".btn-lg").text().appendTo($(this).closest("td"));
-				$(".btn-lg").remove();
-			} else {
-				$("div.itm-lst").each(function(idx) {
-					if(!$(this).find("button").length) {
-						if(na != "") {
-							na += ", ";
-						}
-						na += (idx+1);
-					}
-				});
-				alert("모든 문제를 풀어주세요!");
-				// alert(na+"번 문제를 풀어주세요!");
+				$(this).html(pa[ $(this).attr("id")]);
 			}
-		});
-	$("#0").show();
-	$(".alert").hide();
+			);
+		}
+		// 문제 재생
+		var nagehts=new Howl( {
+			src: [ "./sounds/Reihe 4/r4 B1.mp3"],
+			sprite: {
+				"0": [801, 58913],
+				"1": [14079, 1820],
+				"2": [53750, 1605],
+				"3": [49218, 1693],
+				"4": [27013, 1671],
+				"5": [57832, 1868],
+				"6": [18420, 1774],
+				"7": [22867, 1438],
+				"8": [40766, 1458],
+				"9": [44968, 1510],
+				"10": [31570, 1313],
+				"11": [9974, 1550],
+				"12": [35653, 1896]
+			}
+			,
+			html5: true,
+			volume: 1,
+			format: "mp3",
+			preload: true,
+			onloaderror: function() {
+				$(".alert").append("<br /><strong class=\"font-weight-bold text-dark h4\">페이지를 다시 읽어주시기 바래요.</strong>");
+				console.log("다시 읽어주세요!");
+			}
+			,
+			onload: function() {
+				<?php include "wahl.php";
+				?> // 정답확인
+				$("#chk").on("click", function() {
+					var na="";
+					if($("#itms").find("button").length < 1) {
+						$(".tran").show();
+						$(".itm-lst").each(function() {
+							$(this).html($(this).find("button").html());
+							$(this).addClass("text-success font-weight-bold");
+							// $(this).addClass("font-weight-bold bg-white border rounded border-dark");
+						}
+						);
+						// 정답 확인 div 상자 배경색 속성 없애기
+						$(this).removeClass("btn-light ");
+						var qa=$(".itm-lst").length; // 전체 문항 수
+						var qr=$(".text-success").length; // 맞춘 항목 수
+						var pe=(qr / qa) * 100; // 정답 비율
+						var tcl="white"; // 기본 문자색
+						// 분류 기준은 100%, 80%, 60%, 40%
+						if(pe > 99) {
+							var st="원어민이세요?";
+							var cl="lime";
+							var tcl="dark";
+						}
+						else if(pe > 74) {
+							var st="어! 좀 하시는데요~^^";
+							var cl="success";
+						}
+						else if(pe > 49) {
+							var st="쓰읍~ 다시 해 보실까요?";
+							var cl="primary";
+						}
+						else {
+							var st="좀 더 분발해 주세요~";
+							var cl="danger";
+						}
+						$(this).addClass("btn-"+ cl + " text-"+ tcl);
+						$(this).html("<h4>"+ qa + "문제 중 "+ qr + "개를 맞히셨네요!<br>"+ st + "</h4>");
+					}
+					else {
+						$("div.itm-lst").each(function(idx) {
+							if( !$(this).find("button").length) {
+								if(na !="") {
+									na +=", ";
+								}
+								na +=(idx+1);
+							}
+						}
+						);
+						alert(na+"번 문제를 풀어주세요!");
+					}
+				}
+				);
+				$("#0").show();
+				$(".alert").hide();
+				$(".so").on("click", function() {
+					var t=$(this);
+					var ti=t.attr("id");
+					if (($("div#last").text()==""|| t.text()=="❚❚") && !t.hasClass(".itm-lst")) {
+						$("#last").text(ti);
+						t.text("■");
+						nagehts.seek();
+						nagehts.play(ti);
+						sen[ ti]++;
+						last=ti;
+						$("#cnt-"+ ti).text(sen[ ti]);
+					}
+					else if (last==ti && nagehts.playing($("div#last").text())) {
+						$("#last").text("");
+						t.html(pa[ ti]);
+						nagehts.pause();
+						sen[ ti]--;
+						$("#cnt-"+ ti).text(sen[ ti]);
+					}
+				}
+				);
+				$("#0").show();
+			}
+			,
+			onend: function() {
+				$("div#last").text("");
+				stopAll();
+				$("#cnt-"+last).text(sen[last]);
+				if(last==0) {
+					if(sen[last]==2) {
+						$(".tran").show();
+						$(".so").each(function() {
+							pa[last]=$("#"+last).html();
+						}
+						);
+					}
+				}
+				else if(sen[last]==2) {
+					if($(this).hasClass(".itm")) {
+						$("#"+last).find(".tran").show();
+					}
+					$("#"+last).closest("tr").find(".tran").show();
+					pa[last]=$("#"+last).html();
+				}
+			}
+		}
+		);
 	}
-	});
-	
-	});
-		
+	);
+
 	</script>
-	<!-- ion.sound finished -->
 	<? } ?>
-	<?php include "footer.php"; ?>
+	<?php include"footer.php"; ?>
 </body>
 </html>

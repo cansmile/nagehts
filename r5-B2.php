7e61cc2fb15b2c6e3352326f26f57b2005dcac15
cansmile <cansmile@gmail.com>
2020-03-26 13:50:28 +0900

    R5 B2 수정 반영
    howler로 전환, 선 없는 표 적용

--- r5-B2.php
+++ r5-B2.php
@@ -14,8 +14,6 @@
 					<small>듣고 따라 하세요.</small>
 					<button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so" id="0">
 					HV
-					</button><button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so" id="0_p">
-					❚❚
 					</button>
 					</h2>
 					<h3>[ <small><button type="button" class="btn disabled btn-sm btn-<?php echo($color); ?>">HV</button> 버튼 또는 <button type="button" class="so btn btn-sm btn-outline-secondary disabled">▶</button> 버튼을 눌러 듣기를 2번 완료하면 문장의 번역이 나옵니다.</small> ]</h3>
@@ -23,7 +21,7 @@
 			</div>
 			<div class="row">
 				<div class="col">
-					<table class="table">
+					<table class="table table-borderless">
 						<tbody>
 							<tr>
 								<th scope="row"><button type="button" id="1" class="so btn btn-outline-info">▶</button></th>
@@ -56,163 +54,106 @@
 		</div>
 	</section>
 	
+	<div id="last" class="d-none"></div>
 	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="./js/jquery-3.4.1.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="./js/popper.min.js"></script>
 	<script src="./js/bootstrap.js"></script>
-	<script src="./js/taptogroup.js"></script>
-	<!-- interact.min.js -->
-	<script src="./js/ion.sound.min.js"></script>
+	<script src="./js/howler.core.js"></script>
 	<script>
-		$("#0").hide();
-		$("#0_p").hide();
 		$(".tran").hide();
-		$(document).ready(function() {
-			// 각 문장 재생 횟수 초기화
-			var hm=new Array(), sen=new Array();
-			for(i=0;
-			i < $(".so").length;
-			i++) {
-				hm[i]=0;
-				sen[i]=0;
-			}
-			ion.sound( {
-				sounds : [ {
-					name : "r5 B2 Was trinkst du gern",
-					sprite : {
-						"0": [4.5, 29.1],
-							"1": [12.3, 2.3],
-							"2": [17, 2.5],
-							"3": [19.6, 2.8],
-							"4": [23.2, 2.1],
-							"5": [27.8, 3.2],
-							"6": [31.2, 2.3]
-					}
-				}
-				, {
-					name: "dingdongdang",
-						path: "sounds/"
-				}
-				, {
-					name: "Cartoon_Boing",
-						path: "sounds/"
-				}
-				],
-				path : "sounds/Reihe 5/",
-				preload : true,
-				volume : 1.0,
-				multiplay: false,
-				ended_callback: function(obj) {
-					// 재상이 끝날 때 2번 이상이면 번역 보이기
-					hmn=obj.part;
-					hm[hmn]++;
-					// 전체 재생 끝나면 일시정지 버튼 숨기고 HV 버튼 보이기
-					if(obj.part=="0") {
-						$("#0").show();
-						$("#0_p").hide();
-						if(hm[hmn] > 1) {
-							$(".tran").show();
-						}
-					}
-					else {
-						$("#"+obj.part).html("▶");
-						if(hm[hmn] > 1) {
-							$("#"+hmn).closest("tr").find(".tran").show();
-						}
-					}
-				}
-				, ready_callback: function () {
-					$(".o").on("click", function() {
-						ion.sound.play("dingdongdang");
-					}
-					);
-					$(".x").on("click", function() {
-						ion.sound.play("Cartoon_Boing");
-					}
-					);
-					$("[data-toggle='popover']").popover( {
-						delay : {
-							'hide': 1000
-						}
-						,
-						container : "body"
-					}
-					);
-					$(".pop").click(function () {
-						// 가장 먼저 지문에 'an' 넣기
-						if ( !$(this).siblings().hasClass("an")) {
-							$(this).addClass("an");
-							$(this).addClass("btn-warning");
-							$(this).parent().children().removeClass("btn-light");
-						}
-						;
-						// 문제 풀이 정도 업데이트
-						var perc=Math.round(($(".an").length / $(".q").length) * 100);
-						$(".progress>.bar").attr("width", perc + "%;");
-					}
-					);
-					// 팝업 내용 사라지기
-					$(".pop").popover().click(function() {
-						setTimeout(function() {
-							$(".pop").popover('hide');
-						}
-						, 500);
-					}
-					);
-					$(".so").on("click", function () {
-						if($(this).attr("id").substr(-2)=="_p") {
-							// _p 붙어 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기
-							ion.sound.pause("r5 B2 Was trinkst du gern", {
-								part: "0"
-							}
-							);
-							$("#0").show();
-							$(this).hide();
-						}
-						else if($(this).html()=="▶") {
-							// 재생되고 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기
-							ion.sound.play("r5 B2 Was trinkst du gern", {
-								part: $(this).attr("id")
-							}
-							);
-							$(this).html("❚❚");
-						}
-						else if($(this).html()=="❚❚") {
-							// 재생되고 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기
-							ion.sound.pause("r5 B2 Was trinkst du gern", {
-								part: $(this).attr("id")
-							}
-							);
-							$(this).html("▶");
-						}
-						else {
-							// _p 붙어 있지 않으면 id 그대로 재생
-							ion.sound.play("r5 B2 Was trinkst du gern", {
-								part: $(this).attr("id")
-							}
-							);
-							// 전체 듣기 재생일 때는 일시정지 버튼 보이기
-							if($(this).attr("id")=="0") {
-								$(this).hide();
-								$("#0_p").show();
-							}
-							;
-						}
-						;
-					}
-					);
-					$("#0").show();
-					$(".alert").hide();
-				}
+		// 소리 출력 전역 변수와 함수
+		var sen=new Array(), pa=new Array(), he=new Array(), last;
+		$(".so").each(function() {
+			var t=$(this);
+			var ti=t.attr("id");
+			sen[ti]=0;
+			pa[ti]=t.html();
+			he[ti]=t.height();
+		}
+		);
+		function stopAll() {
+			$(".so").each(function() {
+				$(this).html(pa[$(this).attr("id")]);
 			}
 			);
+		}
+		var nagehts=new Howl( {
+			src: ["./sounds/Reihe 5/r5 B2.mp3"],
+			sprite : {
+				"0": [2466, 30435],
+				"1": [12209, 1743],
+				"2": [16890, 2265],
+				"3": [19326, 2764],
+				"4": [23290, 1755],
+				"5": [27722, 3055],
+				"6": [30945, 2032]
+			}
+			,
+			html5: true,
+			volume: 1,
+			format: "mp3",
+			preload: true,
+			onloaderror: function() {
+				$(".alert").append("<br /><strong class=\"font-weight-bold text-dark display-4\">페이지를 다시 읽어주시기 바래요.</strong>");
+				console.log("다시 읽어주세요!");
+			}
+			,
+			onload: function() {
+				// 음성 준비되면 HV 버튼 나타내기
+				$("#whole").show();
+				$("#alt").hide();
+				// $(".alert").hide();
+				$(".so").on("click", function(idx) {
+					var t=$(this);
+					var ti=t.attr("id");
+					if($("div#last").text()==""|| t.text()=="❚❚") {
+						$("#last").text(ti);
+						t.text("■");
+						nagehts.seek();
+						nagehts.play(ti);
+						sen[ti]++;
+						last=ti;
+						$("#cnt-"+ti).text(sen[ti]);
+					}
+					else if(last==ti && nagehts.playing($("div#last").text())) {
+						$("#last").text("");
+						t.html(pa[ti]);
+						nagehts.pause();
+						sen[ti]--;
+						$("#cnt-"+ti).text(sen[ti]);
+					}
+				}
+				);
+			}
+			,
+			onend: function() {
+				$("div#last").text("");
+				stopAll();
+				$("#cnt-"+last).text(sen[last]);
+				if(last==0) {
+					if(sen[last]==2) {
+						$(".tran").show();
+						$(".so").each(function() {
+							pa[last]=$("#"+last).html();
+						}
+						);
+					}
+				}
+				else if(sen[last]==2) {
+					if($("#"+last).hasClass("itm")) {
+						$("#"+last+">.tran").show();
+					}
+					$("#"+last).closest("tr").find(".tran").show();
+					pa[last]=$("#"+last).html();
+				}
+			}
 		}
 		);
 
 	</script>
-	<!-- ion.sound finished -->
 	<? } ?>
 	<?php include "footer.php"; ?>
 </body>

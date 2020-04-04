ba323dac148846c3b46fc1e9c9312f476ac6285d
cansmile <cansmile@gmail.com>
2020-04-04 22:52:09 +0900

    뷰포트 수정
    ios에서 줄이기 금지 추가

--- header.php
+++ header.php
@@ -9,7 +9,7 @@ $root = "dev/";
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=.5">
+		<meta name="viewport" content="width=device-width, initial-scale=.5, minimum-scale=.25, user-scalable=yes, shrink-to-fit=no">
 		<title>Na geht's? - Willkommen auf der PUTS Lern-Website! PUTS</title>
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 		<link href="./<?php echo($root); ?>css/bootstrap.min.css" rel="stylesheet" type="text/css">

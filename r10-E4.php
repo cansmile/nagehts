eee1234edb765a1d32d05c56e9f7a18ea9ac905b
cansmile <cansmile@gmail.com>
2020-04-07 16:15:36 +0900

    R10 E4 수정
    div 닫는 태그 제거

--- r10-E4.php
+++ r10-E4.php
@@ -1,151 +1,150 @@
 <?php include "header.php"; ?>
-	<body>
-<?php include "nav.php"; ?>
-<!-- 보기시작 -->
-<section class="bg-white rounded p-2" style="position: fixed; left: 0; bottom: 0; z-index: 9999; width: 100%;" id="wahl">
-	<div class="container">
-		<div class="row">
-			<div class="col display-4 bg-<?php echo($color); ?> rounded text-center text-white font-weight-bold col-12">Wahl</div>
-			<div class="col-12" id="itms">
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="1">
+<body>
+	<?php include "nav.php"; ?>
+	<!-- 보기시작 -->
+	<section class="bg-white rounded p-2" style="position: fixed; left: 0; bottom: 0; z-index: 9999; width: 100%;" id="wahl">
+		<div class="container">
+			<div class="row">
+				<div class="col display-4 bg-<?php echo($color); ?> rounded text-center text-white font-weight-bold col-12">Wahl</div>
+				<div class="col-12" id="itms">
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="1">
 					auf das Rathaus
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="2">
-					auf dem Hauptbahnhof 
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="3">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="2">
+					auf dem Hauptbahnhof
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="3">
 					auf dem Marktplatz
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="4">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="4">
 					auf dem Rathaus
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="5">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="5">
 					auf dem Turm
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="6">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="6">
 					auf den Hauptbahnhof
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="7">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="7">
 					auf den Marktplatz
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="8">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="8">
 					auf den Turm
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="9">
-					auf der Bank 
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="10">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="9">
+					auf der Bank
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="10">
 					auf der Post
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="11">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="11">
 					auf die Bank
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="12">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="12">
 					auf die Post
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="13">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="13">
 					im Supermarkt
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="14">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="14">
 					im Café
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="15">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="15">
 					im Fotostudio
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="16">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="16">
 					im Kaufhaus
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="17">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="17">
 					im Park
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="18">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="18">
 					im Reisebüro
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="19">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="19">
 					im Rottannenweg
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="20">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="20">
 					im Schwimmbad
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="21">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="21">
 					im Stadtmuseum
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="22">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="22">
 					in dem Bus
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="23">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="23">
 					in den Bus
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="24">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="24">
 					in den Park
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="25">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="25">
 					in den Rottannenweg
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="26">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="26">
 					in der Apotheke
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="27">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="27">
 					in der Bäckerei
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="28">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="28">
 					in der Frauenkirche
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="29">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="29">
 					in der Haupstraße
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="30">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="30">
 					in die Apotheke
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="31">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="31">
 					in die Bäckerei
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="32">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="32">
 					in die Frauenkirche
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="33">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="33">
 					in die Haupstraße
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="34">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="34">
 					ins Café
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="35">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="35">
 					ins Fotostudio
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="36">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="36">
 					ins Kaufhaus
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="37">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="37">
 					ins Reisebüro
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="38">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="38">
 					ins Schwimmbad
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="39">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="39">
 					ins Stadtmuseum
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="40">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm" id="40">
 					zwischen der Kirche und der Bibliothek
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="41">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="41">
 					zwischen die Kirche und die Bibliothek
-				</button>
-				<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="42">
+					</button>
+					<button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm" id="42">
 					in den Supermarkt
-				</button>
+					</button>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
-
-		<section>
-			<div class="container">
-							<!-- 고르는 아이템들 -->
-				<div class="row">
-					<div class="col-lg-12 mb-4 mt-2 text-center">
-						<h2>[ <small>단어를 알맞은 위치에 넣으세요.</small> ]</h2>
-					</div>
+	</section>
+	<section>
+		<div class="container">
+			<!-- 고르는 아이템들 -->
+			<div class="row">
+				<div class="col-lg-12 mb-4 mt-2 text-center">
+					<h2>[ <small>단어를 알맞은 위치에 넣으세요.</small> ]</h2>
 				</div>
+			</div>
 			<!-- 리스트  시작 -->
 			<div class="row" id="lsts">
 				<div style="min-height: 240px;" class="rounded-top border bg-danger border-white itm-lst col-6 p-2" id="lst-1">
@@ -155,113 +154,112 @@
 					<h2 class="btn btn-xl bg-white ttl d-block">Wohin? (Präposition + Akkusativ)</h2>&nbsp;
 				</div>
 			</div>
-				<!-- 정답화인 버튼 시작 -->
-				<div class="row">
-					<div class="btn my-3 btn-light col-sm-12 col-md-12 col-lg-12" id="chk">
-						정답확인
-					</div>
+			<!-- 정답화인 버튼 시작 -->
+			<div class="row">
+				<div class="btn my-3 btn-light col-sm-12 col-md-12 col-lg-12" id="chk">
+					정답확인
 				</div>
-				<!-- 정답확인 버튼 끝 -->
 			</div>
+			<!-- 정답확인 버튼 끝 -->
 		</div>
-		<!-- 리스트 끝 -->
-		</section>
+	<!-- 리스트 끝 -->
+</section>
+<div id="marg" class="border-danger"></div>
 
-<div id="marg" class="border-danger"></div>		
-		
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="./<?php echo($root); ?>js/jquery-3.4.1.min.js"></script>
-		<!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="./<?php echo($root); ?>js/popper.min.js"></script>
-		<script src="./<?php echo($root); ?>js/bootstrap.js"></script>
-		<script src="./<?php echo($root); ?>js/taptogroup.js"></script>
-		<!-- interact.min.js -->
-		<script src="./<?php echo($root); ?>js/ion.sound.min.js"></script>
-		<script>
-		$("#0").hide();
-		$("#0_p").hide();
-		$(".tran").hide();
-		$("#chk").hide();
-		$(document).ready(function() {
-					// 정답확인
-					$("#chk").on("click", function() {
-						var na="";
-						if($("#itms").find("button").length < 1) {
-							$(".tran").show();
-
-						// 정답 확인 div 상자 배경색 속성 없애기
-						$(this).removeClass("btn-light ");
-
-						$(".itm-lst").each(function() {
-							if($(this).find(".itm")) {
-								$(this).find(".itm").addClass("text-success font-weight-bold");
-							}
-						});
-
-						var qa = $(".itm").length; // 전체 문항 수
-						var qr = $(".text-success").length; // 맞춘 항목 수
-						var pe = (qr / qa) * 100; // 정답 비율
-						var tcl = "white"; // 기본 문자색
-
-						// 분류 기준은 100%, 80%, 60%, 40%
-						if(pe > 99) {
-							var st = "원어민이세요?";
-							var cl = "lime";
-							var tcl = "dark";
-						} else if(pe > 74) {
-							var st = "어! 좀 하시는데요~^^";
-							var cl = "success";
-						} else if(pe > 49) {
-							var st = "쓰읍~ 다시 해 보실까요?";
-							var cl = "primary";
-						} else {
-							var st = "좀 더 분발해 주세요~";
-							var cl = "danger";
-						}
-
-						$(this).addClass("btn-" + cl + " text-" + tcl);
-						$(this).html("<h4>" + qa + "문제 중 " + qr + "개를 맞히셨네요!<br>" + st + "</h4>");
-
-							$(".btn-lg").text().appendTo($(this).closest("td"));
-							$(".btn-lg").remove();
-						}
-						else {
-							$("div.itm-lst").each(function(idx) {
-								if( !$(this).find("button").length) {
-									if(na !="") {
-										na +=", ";
-									}
-									na +=(idx+1);
-								}
-							}
-							);
-							alert("모든 문제를 풀어주세요!");
-							// alert(na+"번 문제를 풀어주세요!");
-						}
-					}
-					);
-					$("#0").show();
-					$(".alert").hide();
-
-					<?php include "wahl.php"; ?>
-
-					var pan = new Array(), pann;
-					// pan = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
-					pan = [];
-					for(var p = 0; p < pan.length; p++) {
-						pann = "#"+pan[p];
-						for(var i = 0; i < $(".itm-lst").length; i++) {
-							if($(pann).hasClass("ans" + (i+1))) {
-								$(pann).insertAfter("#lst-" + (i+1) + ">h2");
-							}
-						}
-						// $(pann).find(".tran").show();
-					}
-					$(".itm-lst>button").addClass("btn-block btn-light");
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="./<?php echo($root); ?>js/jquery-3.4.1.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="./<?php echo($root); ?>js/popper.min.js"></script>
+<script src="./<?php echo($root); ?>js/bootstrap.js"></script>
+<script src="./<?php echo($root); ?>js/taptogroup.js"></script>
+<!-- interact.min.js -->
+<script src="./<?php echo($root); ?>js/ion.sound.min.js"></script>
+<script>
+$("#0").hide();
+$("#0_p").hide();
+$(".tran").hide();
+$("#chk").hide();
+$(document).ready(function() {
+	// 정답확인
+	$("#chk").on("click", function() {
+		var na="";
+		if($("#itms").find("button").length < 1) {
+			$(".tran").show();
+			// 정답 확인 div 상자 배경색 속성 없애기
+			$(this).removeClass("btn-light ");
+			$(".itm-lst").each(function() {
+				if($(this).find(".itm")) {
+					$(this).find(".itm").addClass("text-success font-weight-bold");
+				}
+			}
+			);
+			var qa=$(".itm").length; // 전체 문항 수
+			var qr=$(".text-success").length; // 맞춘 항목 수
+			var pe=(qr / qa) * 100; // 정답 비율
+			var tcl="white"; // 기본 문자색
+			// 분류 기준은 100%, 80%, 60%, 40%
+			if(pe > 99) {
+				var st="원어민이세요?";
+				var cl="lime";
+				var tcl="dark";
+			}
+			else if(pe > 74) {
+				var st="어! 좀 하시는데요~^^";
+				var cl="success";
+			}
+			else if(pe > 49) {
+				var st="쓰읍~ 다시 해 보실까요?";
+				var cl="primary";
+			}
+			else {
+				var st="좀 더 분발해 주세요~";
+				var cl="danger";
+			}
+			$(this).addClass("btn-"+ cl + " text-"+ tcl);
+			$(this).html("<h4>"+ qa + "문제 중 "+ qr + "개를 맞히셨네요!<br>"+ st + "</h4>");
+			$(".btn-lg").text().appendTo($(this).closest("td"));
+			$(".btn-lg").remove();
 		}
-		);
+		else {
+			$("div.itm-lst").each(function(idx) {
+				if( !$(this).find("button").length) {
+					if(na !="") {
+						na +=", ";
+					}
+					na +=(idx+1);
+				}
+			}
+			);
+			alert("모든 문제를 풀어주세요!");
+			// alert(na+"번 문제를 풀어주세요!");
+		}
+	}
+	);
+	$("#0").show();
+	$(".alert").hide();
+	<?php include "wahl.php";
+	?> var pan=new Array(), pann;
+	// pan = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+	pan=[];
+	for(var p=0;
+	p < pan.length;
+	p++) {
+		pann="#"+pan[p];
+		for(var i=0;
+		i < $(".itm-lst").length;
+		i++) {
+			if($(pann).hasClass("ans"+ (i+1))) {
+				$(pann).insertAfter("#lst-"+ (i+1) + ">h2");
+			}
+		}
+		// $(pann).find(".tran").show();
+	}
+	$(".itm-lst>button").addClass("btn-block btn-light");
+}
+);
+
 </script>
-		<!-- ion.sound finished -->
+<!-- ion.sound finished -->
 <?php include "footer.php"; ?>
-	</body>
+</body>
 </html>

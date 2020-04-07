92d5a34c00b1efdd7c522f30130b2f6c620ef5a8
cansmile <cansmile@gmail.com>
2020-04-07 16:27:55 +0900

    R10 E11 수정
    div 닫는 태그 제거

--- r10-E11.php
+++ r10-E11.php
@@ -1,6 +1,6 @@
 <?php include "header.php"; ?>
 <body>
-<?php include "nav.php"; ?>
+	<?php include "nav.php"; ?>
 	<section>
 		<div class="container">
 			<div class="row">
@@ -31,22 +31,19 @@
 				<div class="col-12 border border-dark rounded m-4 p-3" style="line-height: 250%;">
 					<div class="input-group">
 						Sonntag 16 Juli, Am Sonntag, den 16. Juli habe ich einen Stadtrundgang <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 50px;" aria-label="." id="qst-1">.<span class="tran">&nbsp;<small>7월 16일 일요일에 시내구경을 했다.</small>&nbsp;</span> Ich habe die Semperoper und dann den Zwinger <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 50px;" aria-label="." id="qst-2">.<span class="tran">&nbsp;<small>잼퍼오퍼와 츠빙어를 관람하였다.</small>&nbsp;</span> Am Nachmittag habe ich einen Ausflug in die Sächsische Schweiz <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 50px;" aria-label="." id="qst-3">.<span class="tran">&nbsp;<small>오후에는 작센의 스위스로 소풍을 갔다.</small>&nbsp;</span> Dort <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 50px;" aria-label="." id="qst-4"> es ein schönes Panorama.<span class="tran">&nbsp;<small>그곳은 경치가 아름다웠다.</small>&nbsp;</span> Am Abend <input type="text" class="form-control q rounded-0 border-left-0 border-right-0 border-top-0 mx-1 px-2 py-1 text-center border-dark col-sm-12 col-md-6 col-md col-lg" style="position: relative; top: -6px; max-width: 100px; min-width: 50px;" aria-label="." id="qst-5"> ich auf dem Orgelkonzert in der Frauenkirche.<span class="tran">&nbsp;<small>저녁엔 프라우엔교회에서 열린 오르간콘서트에 참석했다…</small>&nbsp;</span>
-					
+						
 					</div>
 				</div>
 			</div>
-				<!-- 정답화인 버튼 시작 -->
-				<div class="row">
-					<div class="btn my-3 btn-light col-sm-12 col-md-12 col-lg-12" id="chk">
-						정답확인
-					</div>
+			<!-- 정답화인 버튼 시작 -->
+			<div class="row">
+				<div class="btn my-3 btn-light col-sm-12 col-md-12 col-lg-12" id="chk">
+					정답확인
 				</div>
-				<!-- 정답확인 버튼 끝 -->
 			</div>
+			<!-- 정답확인 버튼 끝 -->
 		</div>
 	</section>
-	
-	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="./<?php echo($root); ?>js/jquery-3.4.1.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -58,11 +55,10 @@
 	<script>
 		$("#0").hide();
 		$("#0_p").hide();
-
 		$(".tran").hide();
 		$(".ant").hide();
 		var an=new Array();
-		var an=["gemacht","besichtigt","gemacht","gab","war"];
+		var an=["gemacht", "besichtigt", "gemacht", "gab", "war"];
 		$(document).ready(function() {
 			// 값 확인해보자, io값이 참이면 전체 검사
 			function rfchk(th, io) {
@@ -102,7 +98,6 @@
 					}
 				}
 			}
-
 			$(".q").on("keyup", function () {
 				$(this).removeClass("bg-danger");
 				$(this).removeClass("bg-success");
@@ -177,7 +172,6 @@
 			}
 			)
 			/* 입력하는 문자 확인(정답 표시 없음) 여기까지 */
-
 			$("#chk").on("click", function() {
 				var na="";
 				var ri=0;
@@ -206,29 +200,28 @@
 							$("#qst-"+(i+1)).attr("disabled", true);
 							$("#qst-"+(i+1)).addClass("bg-danger text-white rounded font-weight-bold p-1 px-2 ml-1");
 							$("#qst-"+(i+1)).removeClass("rounded-0");
-
 							if( !$.isArray(an[i])) {
 								$("#qst-"+(i+1)).after("<div class=\"d-block text-dark bg-warning rounded p-1 m-1 px-2 font-weight-bold\" style=\"position: relative; top: -6px; max-width: 100px;\">"+an[i]+"</div>");
 							}
 							else {
 								// 2 이상인 경우
-								var r = "<div class=\"d-block text-dark bg-warning rounded p-1 m-1 px-2 font-weight-bold\" style=\"position: relative; top: -6px; max-width: 100px;\">";
-								for(var fd = (an[i].length-1);
-								fd >= 0;
+								var r="<div class=\"d-block text-dark bg-warning rounded p-1 m-1 px-2 font-weight-bold\" style=\"position: relative; top: -6px; max-width: 100px;\">";
+								for(var fd=(an[i].length-1);
+								fd >=0;
 								fd--) {
 									if(fd < (an[i].length-1)) {
-										r = r + " / ";
+										r=r + " / ";
 									}
-									r = r + an[i][fd];
+									r=r + an[i][fd];
 								}
-								r = r +"</div>";
+								r=r +"</div>";
 								if($("span.sen").length > 0) {
 									$("#qst-"+(i+1)).closest("span.sen").after(r);
-								} else {
+								}
+								else {
 									$("#qst-"+(i+1)).after(r);
 								}
 							}
-
 						}
 						if($("#qst-"+(i+1)).hasClass("bg-success")) {
 							ri++;
@@ -236,30 +229,30 @@
 					}
 					// 정답 확인 div 상자 배경색 속성 없애기
 					$(this).removeClass("btn-light ");
-
-					var qa = $(".q").length; // 전체 문항 수
-					var qr = $(".bg-success").length; // 맞춘 항목 수
-					var pe = (qr / qa) * 100; // 정답 비율
-					var tcl = "white"; // 기본 문자색
-
+					var qa=$(".q").length; // 전체 문항 수
+					var qr=$(".bg-success").length; // 맞춘 항목 수
+					var pe=(qr / qa) * 100; // 정답 비율
+					var tcl="white"; // 기본 문자색
 					// 분류 기준은 100%, 80%, 60%, 40%
 					if(pe > 99) {
-						var st = "원어민이세요?";
-						var cl = "lime";
-						var tcl = "dark";
-					} else if(pe > 74) {
-						var st = "어! 좀 하시는데요~^^";
-						var cl = "success";
-					} else if(pe > 49) {
-						var st = "쓰읍~ 다시 해 보실까요?";
-						var cl = "primary";
-					} else {
-						var st = "좀 더 분발해 주세요~";
-						var cl = "danger";
+						var st="원어민이세요?";
+						var cl="lime";
+						var tcl="dark";
 					}
-
-					$(this).addClass("btn-" + cl + " text-" + tcl);
-					$(this).html("<h4>" + qa + "문제 중 " + qr + "개를 맞히셨네요!<br>" + st + "</h4>");
+					else if(pe > 74) {
+						var st="어! 좀 하시는데요~^^";
+						var cl="success";
+					}
+					else if(pe > 49) {
+						var st="쓰읍~ 다시 해 보실까요?";
+						var cl="primary";
+					}
+					else {
+						var st="좀 더 분발해 주세요~";
+						var cl="danger";
+					}
+					$(this).addClass("btn-"+ cl + " text-"+ tcl);
+					$(this).html("<h4>"+ qa + "문제 중 "+ qr + "개를 맞히셨네요!<br>"+ st + "</h4>");
 					$(this).prop("disabled", true);
 					$(".tran").show();
 					$(this).attr("id", "done");
@@ -271,10 +264,9 @@
 				;
 			}
 			);
-
 			var pan=new Array();
 			// pan=[1,2,3,4,5,6,7,8,9,10];
-			pan = [];
+			pan=[];
 			for(var p=0;
 			p < pan.length;
 			p++) {
@@ -283,8 +275,7 @@
 				$(pann).addClass("bg-success text-white font-weight-bold");
 				$(pann).prop("disabled", true);
 				// $(pann).closest("tr").find(".tran").show();
-			}			
-
+			}
 		}
 		);
 

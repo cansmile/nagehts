26c00347d563ea812f144fe23c66d0ff0f200c08
cansmile <cansmile@gmail.com>
2020-04-30 18:39:17 +0900

    R2 B1 수정 반영
    안내문구 변경

--- r2-B1.php
+++ r2-B1.php
@@ -10,8 +10,7 @@
             <!-- 고르는 아이템들 -->
             <div class="row">
                 <div class="col-lg-12 mb-4 mt-2 text-center">
-                    <h2>Hören Sie und sprechen Sie nach.<br>
-                        <small>듣고 따라 하세요.</small>
+                    <h2>[ <small>듣고 알맞은 단어를 선택하세요.</small> ]
                         <button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so" id="0">
                             HV
                         </button>

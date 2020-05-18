a2503946d047aa61ba00eb308d684263e557974a
cansmile <cansmile@gmail.com>
2020-05-18 18:58:42 +0900

    R8 D5 수정 반영
    글자 굵기 속성 빼고 음원 기능 빼기

--- r8-D5.php
+++ r8-D5.php
@@ -6,38 +6,25 @@
     <?php require_once "ready.php"; ?>
     <!-- 알림 끝 -->
     <!-- 보기시작 -->
-    <section class="bg-white rounded p-2"
-
-        id="wahl">
+    <section class="bg-white rounded p-2" id="wahl">
         <div class="container">
             <div class="row">
-                <div
-                    class="bg-<?php echo($color); ?> wahl_title col-12">
+                <div class="bg-<?php echo($color); ?> wahl_title col-12">
                     Wahl</div>
                 <div class="col-12" id="itms">
-                    <button type="button"
-                        class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm px-2 so"
-                        id="1">
+                    <button type="button" class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm px-2 so" id="1">
                         Aspirin nehmen.
                     </button>
-                    <button type="button"
-                        class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm px-2 so"
-                        id="2">
+                    <button type="button" class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm px-2 so" id="2">
                         Gymnastik machen und mehr Sport treiben.
                     </button>
-                    <button type="button"
-                        class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm px-2 so"
-                        id="3">
+                    <button type="button" class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm px-2 so" id="3">
                         nicht mehr rauchen.
                     </button>
-                    <button type="button"
-                        class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm px-2 so"
-                        id="4">
+                    <button type="button" class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm px-2" id="4">
                         mehr Obst und Gemüse essen.
                     </button>
-                    <button type="button"
-                        class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm px-2 so"
-                        id="4">
+                    <button type="button" class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm px-2 so" id="5">
                         abends Schlaftee trinken.
                     </button>
                 </div>
@@ -51,20 +38,16 @@
             <div class="row">
                 <div class="col-lg-12 mb-4 mt-2 text-center">
                     <h2>[ <small>단어를 선택하고 알맞은 위치의 노란 단추를 누르세요.</small> ]
-                        <button type="button"
-                            class="btn btn-<?php echo($color); ?> ml-2 btn-inline so"
-                            id="0">
+                        <button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so" id="0">
                             HV
-                        </button><button type="button"
-                            class="btn btn-<?php echo($color); ?> ml-2 btn-inline so"
+                        </button><button type="button" class="btn btn-<?php echo($color); ?> ml-2 btn-inline so"
                             id="0_p">
                             ❚❚
                         </button>
                     </h2>
                     <h3>[ <small><button type="button"
                                 class="btn disabled btn-sm btn-<?php echo($color); ?>">HV</button>
-                            버튼 또는 <button type="button"
-                                class="btn btn-sm btn-outline-secondary disabled">단어</button>
+                            버튼 또는 <button type="button" class="btn btn-sm btn-outline-secondary disabled">단어</button>
                             버튼을 눌러 듣기를 2번 완료하면 문장의 번역이 나옵니다.</small> ]</h3>
                 </div>
             </div>
@@ -81,10 +64,8 @@
                             <td>&nbsp;</td>
                             <td>
                                 Sie soll&nbsp;
-                                <div class="itm-lst 1itm d-inline-block"
-                                    id="lst-1" style="min-width: 50%;">
-                                    <h2
-                                        class="btn btn-warning btn-xl ttl d-block">
+                                <div class="itm-lst 1itm d-inline-block" id="lst-1" style="min-width: 50%;">
+                                    <h2 class="btn btn-warning btn-xl ttl d-block">
                                         ▼ </h2>
                                 </div>
                                 <span class="tran"><br><small>그녀는 과일과 채소를 더 많이
@@ -105,10 +86,8 @@
                             <td>&nbsp;</td>
                             <td>
                                 Er soll&nbsp;
-                                <div class="itm-lst 1itm d-inline-block"
-                                    id="lst-2" style="min-width: 50%;">
-                                    <h2
-                                        class="btn btn-warning btn-xl ttl d-block">
+                                <div class="itm-lst 1itm d-inline-block" id="lst-2" style="min-width: 50%;">
+                                    <h2 class="btn btn-warning btn-xl ttl d-block">
                                         ▼ </h2>
                                 </div>
                                 <span class="tran"><br><small>그는 아스피린을 복용해야
@@ -129,10 +108,8 @@
                             <td>&nbsp;</td>
                             <td>
                                 Sie soll&nbsp;
-                                <div class="itm-lst 1itm d-inline-block"
-                                    id="lst-3" style="min-width: 50%;">
-                                    <h2
-                                        class="btn btn-warning btn-xl ttl d-block">
+                                <div class="itm-lst 1itm d-inline-block" id="lst-3" style="min-width: 50%;">
+                                    <h2 class="btn btn-warning btn-xl ttl d-block">
                                         ▼ </h2>
                                 </div>
                                 <span class="tran"><br><small>그녀는 잠이 오는 차를 마셔야
@@ -153,10 +130,8 @@
                             <td>&nbsp;</td>
                             <td>
                                 Er soll&nbsp;
-                                <div class="itm-lst 1itm d-inline-block"
-                                    id="lst-4" style="min-width: 50%;">
-                                    <h2
-                                        class="btn btn-warning btn-xl ttl d-block">
+                                <div class="itm-lst 1itm d-inline-block" id="lst-4" style="min-width: 50%;">
+                                    <h2 class="btn btn-warning btn-xl ttl d-block">
                                         ▼ </h2>
                                 </div>
                                 <span class="tran"><br><small>그는 담배를 피우지 않아야
@@ -178,10 +153,8 @@
                             <td>&nbsp;</td>
                             <td>
                                 Sie soll&nbsp;
-                                <div class="itm-lst 1itm d-inline-block"
-                                    id="lst-5" style="min-width: 50%;">
-                                    <h2
-                                        class="btn btn-warning btn-xl ttl d-block">
+                                <div class="itm-lst 1itm d-inline-block" id="lst-5" style="min-width: 50%;">
+                                    <h2 class="btn btn-warning btn-xl ttl d-block">
                                         ▼ </h2>
                                 </div>
                                 <span class="tran"><br><small>그녀는 체조를 하고 더 많은
@@ -193,8 +166,7 @@
             </div>
             <!-- 정답화인 버튼 시작 -->
             <div class="row">
-                <div class="btn my-3 btn-light col-sm-12 col-md-12 col-lg-12"
-                    id="chk">
+                <div class="btn my-3 btn-light col-sm-12 col-md-12 col-lg-12" id="chk">
                     정답확인
                 </div>
             </div>
@@ -234,7 +206,7 @@
                         "1": [12.72, 1.45],
                         "2": [16.86, 2.68],
                         "3": [21.87, 1.41],
-                        "4": [25.58, 2.11]
+                        "5": [25.58, 2.11]
                     }
                 }, {
                     name: "dingdongdang",
@@ -300,10 +272,10 @@
                                 ".an")
                             .length / $(
                                 ".q").length
-                            ) * 100);
+                        ) * 100);
                         $(".progress>.bar").attr(
                             "width", perc + "%;"
-                            );
+                        );
                     });
                     /* 팝업 내용 사라지기 */
                     $(".pop").popover().click(function () {
@@ -318,48 +290,48 @@
                             .substr(-2) == "_p") {
                             /* _p 붙어 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기 */
                             ion.sound.pause(
-                            "r8 D5", {
-                                part: "0"
-                            });
+                                "r8 D5", {
+                                    part: "0"
+                                });
                             $("#0").show();
                             $(this).hide();
                         } else if ($(this).html() ==
                             "▶") {
                             /* 재생되고 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기 */
                             ion.sound.play(
-                            "r8 D5", {
-                                part: $(
-                                        this)
-                                    .attr(
-                                        "id"
+                                "r8 D5", {
+                                    part: $(
+                                            this)
+                                        .attr(
+                                            "id"
                                         )
-                            });
+                                });
                             $(this).html("❚❚");
                         } else if ($(this).html() ==
                             "❚❚") {
                             /* 재생되고 있는 것은 일시정지 버튼 숨기고 HV 버튼 보이기 */
                             ion.sound.pause(
-                            "r8 D5", {
-                                part: $(
-                                        this)
-                                    .attr(
-                                        "id"
+                                "r8 D5", {
+                                    part: $(
+                                            this)
+                                        .attr(
+                                            "id"
                                         )
-                            });
+                                });
                             $(this).html("▶");
                         } else {
                             /* _p 붙어 있지 않으면 id 그대로 재생 */
                             ion.sound.play(
-                            "r8 D5", {
-                                part: $(
-                                        this)
-                                    .attr(
-                                        "id"
+                                "r8 D5", {
+                                    part: $(
+                                            this)
+                                        .attr(
+                                            "id"
                                         )
-                            });
+                                });
                             /* 전체 듣기 재생일 때는 일시정지 버튼 보이기 */
                             if ($(this).attr(
-                                "id") == "0") {
+                                    "id") == "0") {
                                 $(this).hide();
                                 $("#0_p").show();
                             };
@@ -385,14 +357,14 @@
                                     if ($(this)
                                         .find(
                                             "button.btn"
-                                            )) {
+                                        )) {
                                         $(this)
                                             .find(
                                                 "button.btn"
-                                                )
+                                            )
                                             .addClass(
                                                 "text-success"
-                                                );
+                                            );
                                     }
                                 });
 
@@ -402,9 +374,9 @@
                                     ".text-success")
                                 .length; /* 맞춘 항목 수 */
                             var pe = (qr / qa) *
-                            100; /* 정답 비율 */
+                                100; /* 정답 비율 */
                             var tcl =
-                            "white"; /* 기본 문자색 */
+                                "white"; /* 기본 문자색 */
 
                             /* 분류 기준은 100%, 80%, 60%, 40% */
                             if (pe > 99) {
@@ -444,18 +416,18 @@
                                     if (!$(this)
                                         .find(
                                             "button"
-                                            )
+                                        )
                                         .length
-                                        ) {
+                                    ) {
                                         if (na !=
                                             ""
-                                            ) {
+                                        ) {
                                             na +=
                                                 ", ";
                                         }
                                         na += (idx +
                                             1
-                                            );
+                                        );
                                     }
                                 });
                             alert("모든 문제를 풀어주세요!");
@@ -473,23 +445,11 @@
                     for (var p = 0; p < pan.length; p++) {
                         var pani = "#lst-" + pan[p];
                         $(".itm").each(function () {
-                            if ($(this).hasClass(
-                                    "ans" + pan[p]
-                                    )) {
-                                $("#" + $(this)
-                                        .attr("id"))
-                                    .appendTo($(
-                                        "#lst-" +
-                                        pan[p]));
-                                $("#lst-" + pan[p] +
-                                        ">.itm")
-                                    .addClass(
-                                        "btn-block font-weight-bold"
-                                        );
-                                $("#lst-" + pan[p] +
-                                        ">h2")
-                                    .remove();
-                                /* $("#lst-" + pan[p]).closest("table").find(".tran").show(); */
+                            if ($(this).hasClass("ans" + pan[p])) {
+                                $("#" + $(this).attr("id")).appendTo($("#lst-" + pan[p]));
+                                $("#lst-" + pan[p] + ">.itm").addClass(
+                                    "btn-block");
+                                $("#lst-" + pan[p] + ">h2").remove();
                             }
                         })
                     }

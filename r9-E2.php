eeff4bfb9f753ab1f174ba832bee6f28a367ec1e
cansmile <cansmile@gmail.com>
2020-06-03 21:44:49 +0900

    R9 E2 수정
    howler로 전환

--- r9-E2.php
+++ r9-E2.php
@@ -35,7 +35,8 @@
                 <div class="col-sm-12 col-md-12 col-lg-6">
                     <div class="input-group lh350">
                         <input type="text" class="form-control q pl-0 col-2 rounded-0 border-bottom-only mx-1 px-2 py-1 text-center
-                            border-dark mx-1 t_add_3" style="min-width: 150px;" aria-label="." id="qst-1">Frühling – Hase –
+                            border-dark mx-1 t_add_3" style="min-width: 150px;" aria-label="." id="qst-1">Frühling –
+                        Hase –
                         Eier –
                         Schokolade
                         <span class="tran"><strong>부활절</strong>&nbsp;<small>봄
@@ -45,7 +46,8 @@
                 <div class="col-sm-12 col-md-12 col-lg-6">
                     <div class="input-group lh350">
                         <input type="text" class="form-control q pl-0 col-2 rounded-0 border-bottom-only mx-1 px-2 py-1 text-center
-                            border-dark mx-1 t_add_3" style="min-width: 150px;" aria-label="." id="qst-2">Maske – Kostüm –
+                            border-dark mx-1 t_add_3" style="min-width: 150px;" aria-label="." id="qst-2">Maske –
+                        Kostüm –
                         Karnerval
                         – Rosenmontag
                         <span class="tran"><strong>카니발</strong>&nbsp;<small>가면 -
@@ -55,7 +57,8 @@
                 <div class="col-sm-12 col-md-12 col-lg-6">
                     <div class="input-group lh350">
                         <input type="text" class="form-control q pl-0 col-2 rounded-0 border-bottom-only mx-1 px-2 py-1 text-center
-                            border-dark mx-1 t_add_3" style="min-width: 150px;" aria-label="." id="qst-3">Bier – Musik – Brezel
+                            border-dark mx-1 t_add_3" style="min-width: 150px;" aria-label="." id="qst-3">Bier – Musik
+                        – Brezel
                         -
                         München
                         <span class="tran"><strong>옥토버페스트</strong>&nbsp;<small>맥주
@@ -99,8 +102,9 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="./<?php echo($root); ?>js/popper.min.js"></script>
     <script src="./<?php echo($root); ?>js/bootstrap.js"></script>
-    <!-- interact.min.js -->
-    <script src="./<?php echo($root); ?>js/ion.sound.min.js"></script>
+    <script src="./<?php echo($root); ?>js/howler.core.js"></script>
+    <!-- 맞고 틀리는지 소리 -->
+    <?php require_once("./{$root}oxsound.php"); ?>
     <script>
         $(".tran").hide();
         $(".ant").hide();
@@ -109,8 +113,7 @@
             "Geburtstag"
         ];
         $(document).ready(function () {
-            /* 입력하는 문자 확인(정답 표시 없음) 여기부터 */
-            /* 값 확인해보자, io값이 참이면 전체 검사 */
+            /* 입력하는 문자 확인(정답 표시 없음) 여기부터 */ /* 값 확인해보자, io값이 참이면 전체 검사 */
             function rfchk(th, io) {
                 var q, qn, a, b, fl;
                 q = th.val().length;
@@ -142,71 +145,55 @@
                         }
                     }
                 }
-            }
+            };
+
             $(".q").on("keyup", function () {
                 $(this).removeClass("bg-danger");
                 $(this).removeClass("bg-success");
-                $("#ant-" + $(this).attr("id").substr(4))
-                    .removeClass("text-danger");
-                $("#ant-" + $(this).attr("id").substr(4))
-                    .removeClass("text-success");
+                $("#ant-" + $(this).attr("id").substr(4)).removeClass("text-danger");
+                $("#ant-" + $(this).attr("id").substr(4)).removeClass("text-success");
                 if (rfchk($(this))) {
-                    $(this).addClass(
-                        "text-white font-weight-bold");
+                    $(this).addClass("text-white font-weight-bold");
                     $(this).addClass("bg-success");
-                    $("#ant-" + $(this).attr("id").substr(4))
-                        .addClass("text-success");
+                    $("#ant-" + $(this).attr("id").substr(4)).addClass("text-success");
                 } else {
-                    $(this).addClass(
-                        "text-white font-weight-bold");
+                    $(this).addClass("text-white font-weight-bold");
                     $(this).addClass("bg-danger");
-                    $("#ant-" + $(this).attr("id").substr(4))
-                        .addClass("text-danger");
+                    $("#ant-" + $(this).attr("id").substr(4)).addClass("text-danger");
                 }
                 if (!$(this).val()) {
                     $(this).removeClass("bg-danger");
                     $(this).removeClass("bg-success");
-                    $(this).removeClass(
-                        "text-white font-weight-bold");
+                    $(this).removeClass("text-white font-weight-bold");
                 }
                 if ($(this).val()) {
-                    $("#ant-" + $(this).attr("id").substr(4))
-                        .show();
-                    $("#ant-" + $(this).attr("id").substr(4))
-                        .text($(this).val());
+                    $("#ant-" + $(this).attr("id").substr(4)).show();
+                    $("#ant-" + $(this).attr("id").substr(4)).text($(this).val());
                 } else {
-                    $("#ant-" + $(this).attr("id").substr(4))
-                        .hide();
+                    $("#ant-" + $(this).attr("id").substr(4)).hide();
                 }
             });
+
             $(".q").on("focusin", function () {
-                $("#ant-" + $(this).attr("id").substr(4))
-                    .show();
-                if (!$("#ant-" + $(this).attr("id").substr(4))
-                    .text()) {
-                    $("#ant-" + $(this).attr("id").substr(4))
-                        .text($(this).val());
+                $("#ant-" + $(this).attr("id").substr(4)).show();
+                if (!$("#ant-" + $(this).attr("id").substr(4)).text()) {
+                    $("#ant-" + $(this).attr("id").substr(4)).text($(this).val());
                 }
-                if ($("#ant-" + $(this).attr("id").substr(4))
-                    .text()) {
+                if ($("#ant-" + $(this).attr("id").substr(4)).text()) {
                     if (rfchk($(this))) {
-                        $(this).addClass(
-                            "text-white font-weight-bold");
+                        $(this).addClass("text-white font-weight-bold");
                         $(this).addClass("bg-success");
-                        $("#ant-" + $(this).attr("id").substr(
-                            4)).addClass("text-success");
+                        $("#ant-" + $(this).attr("id").substr(4)).addClass("text-success");
                     } else {
-                        $(this).addClass(
-                            "text-white font-weight-bold");
+                        $(this).addClass("text-white font-weight-bold");
                         $(this).addClass("bg-danger");
-                        $("#ant-" + $(this).attr("id").substr(
-                            4)).addClass("text-danger");
+                        $("#ant-" + $(this).attr("id").substr(4)).addClass("text-danger");
                     }
                 }
             });
+
             $(".q").on("focusout", function () {
-                $("#ant-" + $(this).attr("id").substr(4))
-                    .hide();
+                $("#ant-" + $(this).attr("id").substr(4)).hide();
                 if (rfchk($(this), true)) {
                     $(this).addClass("bg-success");
                     $(this).addClass("text-white");
@@ -215,21 +202,19 @@
                 }
                 if ($(this).val()) {
                     if ($(this).hasClass("bg-danger")) {
-                        ion.sound.play("Cartoon_Boing");
+                        x.play();
                     } else if ($(this).hasClass("bg-success")) {
-                        ion.sound.play("dingdongdang");
+                        o.play();
                         $(this).prop("disabled", true);
                     }
                 }
                 $(this).removeClass("bg-danger");
                 if (!$(this).attr("disabled")) {
-                    $(this).removeClass(
-                        "text-white font-weight-bold");
+                    $(this).removeClass("text-white font-weight-bold");
                     $(this).removeClass("bg-success");
                 }
-            })
+            });
             /* 입력하는 문자 확인(정답 표시 없음) 여기까지 */
-
             $("#chk").on("click", function () {
                 var na = "";
                 var ri = 0;
@@ -239,69 +224,49 @@
                         na += ", ";
                     }
                     if ($(this).val() == "") {
-                        na += $(this).attr("id").substr(
-                            4, 1);
+                        na += $(this).attr("id").substr(4, 1);
                     }
                 });
-                if ($(this).attr("id") == "done") {} else if (
-                    na == "") {
+                if ($(this).attr("id") == "done") {} else if (na == "") {
                     for (var i = 0; i < an.length; i++) {
                         var oran = $("#qst-" + (i + 1)).val();
                         if (rfchk($("#qst-" + (i + 1)), true)) {
-                            $("#qst-" + (i + 1)).addClass(
-                                "cr"
-                            );
-                            $("#qst-" + (i + 1)).removeClass(
-                                "rounded-0");
+                            $("#qst-" + (i + 1)).addClass("cr");
+                            $("#qst-" + (i + 1)).removeClass("rounded-0");
                         } else {
                             $("#qst-" + (i + 1)).val(oran);
-                            $("#qst-" + (i + 1)).attr(
-                                "disabled", true);
-                            $("#qst-" + (i + 1)).addClass(
-                                "wa"
-                            );
-                            $("#qst-" + (i + 1)).removeClass(
-                                "rounded-0");
-
+                            $("#qst-" + (i + 1)).attr("disabled", true);
+                            $("#qst-" + (i + 1)).addClass("wa");
+                            $("#qst-" + (i + 1)).removeClass("rounded-0");
                             if (!$.isArray(an[i])) {
-                                $("#qst-" + (i + 1)).after(
-                                    "<div class=\"d-block ra t-6\">" +
-                                    an[i] + "</div>");
+                                $("#qst-" + (i + 1)).after("<div class=\"d-block ra t-6\">" + an[i] +
+                                    "</div>");
                             } else {
                                 /* 2 이상인 경우 */
-                                var r =
-                                    "<div class=\"d-block ra t-6\">";
-                                for (var fd = (an[i].length -
-                                        1); fd >= 0; fd--) {
-                                    if (fd < (an[i].length -
-                                            1)) {
+                                var r = "<div class=\"d-block ra t-6\">";
+                                for (var fd = (an[i].length - 1); fd >= 0; fd--) {
+                                    if (fd < (an[i].length - 1)) {
                                         r = r + " / ";
                                     }
                                     r = r + an[i][fd];
                                 }
                                 r = r + "</div>";
                                 if ($("span.sen").length > 0) {
-                                    $("#qst-" + (i + 1))
-                                        .closest("span.sen")
-                                        .after(r);
+                                    $("#qst-" + (i + 1)).closest("span.sen").after(r);
                                 } else {
-                                    $("#qst-" + (i + 1)).after(
-                                        r);
+                                    $("#qst-" + (i + 1)).after(r);
                                 }
                             }
-
                         }
-                        if ($("#qst-" + (i + 1)).hasClass(
-                                "bg-success")) {
+                        if ($("#qst-" + (i + 1)).hasClass("bg-success")) {
                             ri++;
                         }
                     }
+
                     /* 정답 확인 div 상자 배경색 속성 없애기 */
                     $(this).removeClass("btn-light ");
-
                     var qa = $(".q").length; /* 전체 문항 수 */
-                    var qr = $(".bg-success")
-                        .length; /* 맞춘 항목 수 */
+                    var qr = $(".bg-success").length; /* 맞춘 항목 수 */
                     var pe = (qr / qa) * 100; /* 정답 비율 */
                     var tcl = "white"; /* 기본 문자색 */
 
@@ -320,36 +285,27 @@
                         var st = "좀 더 분발해 주세요~";
                         var cl = "danger";
                     }
-
-                    $(this).addClass("btn-" + cl + " text-" +
-                        tcl);
-                    $(this).html("<h4>" + qa + "문제 중 " + qr +
-                        "개를 맞히셨네요!<br>" + st + "</h4>");
+                    $(this).addClass("btn-" + cl + " text-" + tcl);
+                    $(this).html("<h4>" + qa + "문제 중 " + qr + "개를 맞히셨네요!<br>" + st + "</h4>");
                     $(this).prop("disabled", true);
                     $(".tran").show();
                     $(this).attr("id", "done");
                 } else {
                     alert("모든 문제를 풀어주세요!");
                 };
-
                 $(".tran").show();
             });
-
             var pan = new Array();
-            /* pan=[1,2,3,4,5,6,7,8,9,10]; */
             pan = [1];
             for (var p = 0; p < pan.length; p++) {
                 var pann = "#qst-" + pan[p];
                 $(pann).val(an[(pan[p] - 1)]);
-                $(pann).addClass(
-                    "bg-success text-white font-weight-bold");
+                $(pann).addClass("bg-success text-white font-weight-bold");
                 $(pann).prop("disabled", true);
-                /* $(pann).closest("tr").find(".tran").show(); */
             }
         });
 
     </script>
-    <!-- ion.sound finished -->
     <?php require "footer.php"; ?>
 </body>
 

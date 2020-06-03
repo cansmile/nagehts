cf6703f506ea891785b0d03066af3b323fc6d834
cansmile <cansmile@gmail.com>
2020-06-03 23:19:29 +0900

    R10 C1 수정
    howler로 전환

--- r10-C1.php
+++ r10-C1.php
@@ -212,12 +212,11 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="./<?php echo($root); ?>js/popper.min.js"></script>
     <script src="./<?php echo($root); ?>js/bootstrap.js"></script>
-    <script src="./<?php echo($root); ?>js/taptogroup.js"></script>
-    <!-- interact.min.js -->
-    <script src="./<?php echo($root); ?>js/ion.sound.min.js"></script>
+    <script src="./<?php echo($root); ?>js/taptogrouph.js"></script>
+    <script src="./<?php echo($root); ?>js/howler.core.js"></script>
+    <!-- 맞고 틀리는지 소리 -->
+    <?php require_once("./{$root}oxsound.php"); ?>
     <script>
-        $("#0").hide();
-        $("#0_p").hide();
         $(".tran").hide();
         $("#chk").hide();
         $(document).ready(function () {
@@ -280,7 +279,6 @@
                         }
                     });
                     alert("모든 문제를 풀어주세요!");
-                    /* alert(na+"번 문제를 풀어주세요!"); */
                 }
             });
             $("#0").show();
@@ -289,7 +287,6 @@
             <?php require "wahl.php"; ?>
 
             var pan = new Array();
-            /* pan = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14"]; */
             pan = [1];
             var il = $("#itms>.itm").length;
             for (var p = 0; p < pan.length; p++) {
@@ -301,7 +298,6 @@
                         $("#" + $(this).attr("id")).addClass(
                             "btn-block font-weight-bold");
                         $("#lst-" + pan[p] + ">h2").remove();
-                        /* $("#lst-"+ pan[p]).parent().find(".tran").show(); */
                     }
                 })
             }
@@ -309,7 +305,6 @@
         });
 
     </script>
-    <!-- ion.sound finished -->
     <?php require "footer.php"; ?>
 </body>
 

3f890f40b4f5efd0adbc0b2bbcd0e1faf95b7278
cansmile <cansmile@gmail.com>
2020-06-03 23:36:18 +0900

    R10 E2a 수정
    howler로 전환

--- r10-E2a.php
+++ r10-E2a.php
@@ -316,9 +316,10 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="./<?php echo($root); ?>js/popper.min.js"></script>
     <script src="./<?php echo($root); ?>js/bootstrap.js"></script>
-    <script src="./<?php echo($root); ?>js/taptogroup.js"></script>
-    <!-- interact.min.js -->
-    <script src="./<?php echo($root); ?>js/ion.sound.min.js"></script>
+    <script src="./<?php echo($root); ?>js/taptogrouph.js"></script>
+    <script src="./<?php echo($root); ?>js/howler.core.js"></script>
+    <!-- 맞고 틀리는지 소리 -->
+    <?php require_once("./{$root}oxsound.php"); ?>
     <script>
         $(".tran").hide();
         $("#chk").hide();
@@ -385,14 +386,12 @@
                         }
                     });
                     alert("모든 문제를 풀어주세요!");
-                    /* alert(na+"번 문제를 풀어주세요!"); */
                 }
             });
 
             <?php require "wahl.php"; ?>
 
             var pan = new Array();
-            /* pan = ["1","2","3","4","5","6","7","8","9","10"]; */
             pan = [1, 5, 8, 10];
             var il = $("#itms>.itm").length;
             for (var p = 0; p < pan.length; p++) {
@@ -407,14 +406,12 @@
                         $("#" + $(this).attr("id")).addClass(
                             "btn-light");
                         $("#lst-" + pan[p] + ">h2").remove();
-                        /* $("#lst-" + pan[p]).closest("tr").find(".tran").show(); */
                     }
                 })
             }
         });
 
     </script>
-    <!-- ion.sound finished -->
     <?php require "footer.php"; ?>
 </body>
 

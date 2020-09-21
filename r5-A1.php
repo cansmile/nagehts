fbcbe2c2c98365dc93127e742398b71a9b135c8c
cansmile <cansmile@gmail.com>
2020-09-21 22:38:44 +0900

    R5 AA 수정 반영
    음원 추가

--- r5-A1.php
+++ r5-A1.php
@@ -10,31 +10,31 @@
                     Wahl</div>
                 <div class="col-12" id="itms">
                     <button type="button"
-                        class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm"
+                        class="mt-1 mx-1 btn ans5 btn-lg btn-outline-dark itm so"
                         id="1">
                         im Biergarten
                         <span class="tran"><small>야외 맥주집에서</small></span>
                     </button>
                     <button type="button"
-                        class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm"
+                        class="mt-1 mx-1 btn ans3 btn-lg btn-outline-dark itm so"
                         id="2">
                         im Restaurant
                         <span class="tran"><small>식당에서</small></span>
                     </button>
                     <button type="button"
-                        class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm"
+                        class="mt-1 mx-1 btn ans4 btn-lg btn-outline-dark itm so"
                         id="3">
                         in der Weinstube
                         <span class="tran"><small>와인바에서</small></span>
                     </button>
                     <button type="button"
-                        class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm"
+                        class="mt-1 mx-1 btn ans1 btn-lg btn-outline-dark itm so"
                         id="4">
                         in der Mensa
                         <span class="tran"><small>학생식당에서</small></span>
                     </button>
                     <button type="button"
-                        class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm"
+                        class="mt-1 mx-1 btn ans2 btn-lg btn-outline-dark itm so"
                         id="5">
                         am Imbiss
                         <span class="tran"><small>스낵바에서</small></span>
@@ -180,6 +180,7 @@
     </section>
 
     <div id="marg"></div>
+    <div id="last"></div>
 
     <?php require "footer.php"; ?>
     <script src="./dev/js/taptogrouph.js"></script>
@@ -187,102 +188,167 @@
     <!-- 맞고 틀리는지 소리 -->
     <?php require_once("./dev/oxsound.php"); ?>
     <script>
+        $("#0").hide();
+        $("#last").hide();
         $(".tran").hide();
         $("#chk").hide();
-        $(document).ready(function () {
-            <?php require "wahl.php"; ?>
-            /* 정답확인 */
-            $("#chk").on("click", function () {
-                if ($("#wahl").visibility != "visible" && $(
-                        this).attr("id") == "chk") {
-                    $(this).attr("id", "done");
-                    $(".itm").each(function () {
-                        if ($(this).parent().attr("id")
-                            .length > 5) {
-                            var a = $(this).parent()
-                                .attr("id").substr($(
-                                        this).parent()
-                                    .attr("id").length -
-                                    2, 2);
-                        } else {
-                            var a = $(this).parent()
-                                .attr("id").substr($(
-                                        this).parent()
-                                    .attr("id").length -
-                                    1, 1);
-                        }
-                        $(".tran").show();
-                        if ($(this).hasClass("ans" + (
-                                a))) {
-                            $(this).addClass(
-                                "text-success font-weight-bold"
-                                );
-                        } else {
-                            $(this).addClass(
-                                "text-warning font-weight-bold"
-                                );
-                            $(this).find(".tran")
-                        .show();
-                        };
-                        if ($(this).hasClass(
-                                "text-warning")) {
-                            /* $(this).text().insertAfter($("lst-"+($(this).attr("id").substr(3,)))) */
-                            for (var i = 1; i <= $(
-                                    ".itm-lst")
-                                .length; i++) {
-                                if ($(this).hasClass(
-                                        "ans" + i)) {
-                                    $(eval('"#lst-' +
-                                                i + '"'
-                                                ))
-                                        .append(
-                                            "<button class=\"mt-1 mx-1 btn btn-lg btn-outline-dark btn-block text-danger bg-white font-weight-bold\">" +
-                                            $(this)
-                                            .html() +
-                                            "</button>"
-                                            );
-                                    /* $(lstn).append(i); */
-                                }
-                            }
-                        };
-                    });
-                    /* 정답 확인 div 상자 배경색 속성 없애기 */
-                    $(this).removeClass("btn-light ");
-                    $(".itm-lst").each(function () {
-                        if ($(this).find(".btn")) {
-                            $(this).find(".btn")
-                                .addClass(
-                                    "text-success");
-                        }
-                    });
-                    var qa = $(".itm-lst").length; /* 전체 문항 수 */
-                    var qr = $(".text-success")
-                    .length; /* 맞춘 항목 수 */
-                    var pe = (qr / qa) * 100; /* 정답 비율 */
-                    var tcl = "white"; /* 기본 문자색 */
-                    /* 분류 기준은 100%, 80%, 60%, 40% */
-                    if (pe > 99) {
-                        var st = "원어민이세요?";
-                        var cl = "lime";
-                        var tcl = "dark";
-                    } else if (pe > 74) {
-                        var st = "어! 좀 하시는데요~^^";
-                        var cl = "success";
-                    } else if (pe > 49) {
-                        var st = "쓰읍~ 다시 해 보실까요?";
-                        var cl = "primary";
-                    } else {
-                        var st = "좀 더 분발해 주세요~";
-                        var cl = "danger";
-                    }
-                    $(this).addClass("btn-" + cl + " text-" +
-                        tcl);
-                    $(this).html("<h4>" + qa + "문제 중 " + qr +
-                        "개를 맞히셨네요!<br>" + st + "</h4>");
-                }
-                $('.tran').show();
-            });
+
+        /* 각 문장 재생 횟수 초기화 */
+        var hm = new Array(),
+            sen = new Array();
+        $(".so").each(function () {
+            hm[$(this).attr("id")] = 0;
+            sen[$(this).attr("id")] = $("#" + $(this).attr("id"))
+                .html();
         });
+
+        $(document).ready(function () {{
+            /* 소리 출력 전역 변수와 함수 */
+            var sen = new Array(),
+                pa = new Array(),
+                he = new Array(),
+                last;
+            $(".so").each(function () {
+                var t = $(this);
+                var ti = t.attr("id");
+                sen[ti] = 0;
+                pa[ti] = t.html();
+            });
+
+            function stopAll() {
+                $(".so").each(function () {
+                    $(this).html(pa[$(this).attr("id")]);
+                });
+            } /* 문제 재생 */
+            var nagehts = new Howl({
+                src: ["./dev/sounds/Reihe 5/r5 A1.mp3"],
+                sprite: {
+                    "0": [451, 19978],
+                    "1": [7188, 1818],
+                    "2": [10337, 1564],
+                    "3": [13071, 1891],
+                    "4": [16098, 1574],
+                    "5": [18880, 1549]
+
+                },
+                html5: true,
+                volume: 1,
+                format: "mp3",
+                preload: true,
+                onloaderror: function () {
+                    $(".alert").append(
+                        "<br /><strong class=\"font-weight-bold text-dark display-4\">페이지를 다시 읽어주시기 바래요.</strong>"
+                    );
+                    console.log("다시 읽어주세요!");
+                },
+                onload: function () {
+                    /* 음성 준비되면 HV 버튼 나타내기 */
+                    $("#0").show();
+                    $(".alert").hide();
+                    $(".so").on("click", function () {
+                        var t = $(this);
+                        var ti = t.attr("id");
+                        if (($("div#last").text() == "" || t.text() == "❚❚") && !t.hasClass(
+                            ".itm-lst")) {
+                            $("#last").text(ti);
+                            t.text("■");
+                            nagehts.seek();
+                            nagehts.play(ti);
+                            sen[ti]++;
+                            last = ti;
+                            $("#cnt-" + ti).text(sen[ti]);
+                        } else if (last == ti && nagehts.playing($("div#last").text())) {
+                            $("#last").text("");
+                            t.html(pa[ti]);
+                            nagehts.pause();
+                            sen[ti]--;
+                            $("#cnt-" + ti).text(sen[ti]);
+                        }
+                    });
+
+
+                    <?php require "wahl.php"; ?>
+                    /* 정답확인 */
+                    $("#chk").on("click", function () {
+                        if ($("#wahl").visibility != "visible" && $(this).attr("id") == "chk") {
+                            $(this).attr("id", "done");
+                            $(".itm").each(function () {
+                                if ($(this).parent().attr("id").length > 5) {
+                                    var a = $(this).parent().attr("id").substr($(this).parent().attr("id").length - 2, 2);
+                                } else {
+                                    var a = $(this).parent().attr("id").substr($(this).parent().attr("id").length - 1, 1);
+                                }
+                                $(".tran").show();
+                                if ($(this).hasClass("ans" + (a))) {
+                                    $(this).addClass("text-success font-weight-bold");
+                                } else {
+                                    $(this).addClass("text-warning font-weight-bold");
+                                    $(this).find(".tran").show();
+                                }
+                                ;
+                                if ($(this).hasClass("text-warning")) {/* $(this).text().insertAfter($("lst-"+($(this).attr("id").substr(3,)))) */
+                                    for (var i = 1; i <= $(".itm-lst").length; i++) {
+                                        if ($(this).hasClass("ans" + i)) {
+                                            $(eval('"#lst-' + i + '"')).append("<button class=\"mt-1 mx-1 btn btn-lg btn-outline-dark btn-block text-danger bg-white font-weight-bold\">" + $(this).html() + "</button>");/* $(lstn).append(i); */
+                                        }
+                                    }
+                                }
+                                ;
+                            });/* 정답 확인 div 상자 배경색 속성 없애기 */
+                            $(this).removeClass("btn-light ");
+                            $(".itm-lst").each(function () {
+                                if ($(this).find(".btn")) {
+                                    $(this).find(".btn").addClass("text-success");
+                                }
+                            });
+                            var qa = $(".itm-lst").length; /* 전체 문항 수 */
+                            var qr = $(".text-success").length; /* 맞춘 항목 수 */
+                            var pe = (qr / qa) * 100; /* 정답 비율 */
+                            var tcl = "white"; /* 기본 문자색 *//* 분류 기준은 100%, 80%, 60%, 40% */
+                            if (pe > 99) {
+                                var st = "원어민이세요?";
+                                var cl = "lime";
+                                var tcl = "dark";
+                            } else if (pe > 74) {
+                                var st = "어! 좀 하시는데요~^^";
+                                var cl = "success";
+                            } else if (pe > 49) {
+                                var st = "쓰읍~ 다시 해 보실까요?";
+                                var cl = "primary";
+                            } else {
+                                var st = "좀 더 분발해 주세요~";
+                                var cl = "danger";
+                            }
+                            $(this).addClass("btn-" + cl + " text-" + tcl);
+                            $(this).html("<h4>" + qa + "문제 중 " + qr + "개를 맞히셨네요!<br>" + st + "</h4>");
+                        }
+                        $('.tran').show();
+                    });
+
+                    $("#0").show();
+                    $(".alert").hide();
+                },
+                onend: function () {
+                    $("div#last").text("");
+                    stopAll();
+                    $("#cnt-" + last).text(sen[last]);
+                    if (last == 0) {
+                        if (sen[last] == 2) {
+                            $(".tran").show();
+                            $(".so").each(function () {
+                                pa[last] = $("#" + last).html();
+                            });
+                        }
+                    } else if (sen[last] == 2) {
+                        if ($("#" + last).hasClass("itm")) {
+                            $("#" + last + ">.tran").show();
+                        }
+                        $("#" + last).closest("div").find(".tran").show();
+                        pa[last] = $("#" + last).html();
+                    }
+                }
+            });
+        }});
 
     </script>
     </body>
